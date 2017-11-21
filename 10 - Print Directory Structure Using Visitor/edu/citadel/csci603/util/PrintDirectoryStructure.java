e67b68a4cbede193462b89a297d7200783c4d711
pabaier <pabaier@gmail.com>
2017-11-21 14:05:03 -0500

    just starting

---
+++ 10 - Print Directory Structure Using Visitor/edu/citadel/csci603/util/PrintDirectoryStructure.java
@@ -0,0 +1,68 @@
+package edu.citadel.csci603.util;
+
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.io.IOException;
+import static java.nio.file.FileVisitResult.*;
+
+
+/**
+ * Utility class for computing the number of bytes in a directory tree.
+ */
+public class PrintDirectoryStructure extends SimpleFileVisitor<Path>
+  {
+    /**
+     * Prints the structure for the file whose path name is given in arg[0].
+     */
+    public static void main(String[] args) throws IOException
+      {
+        if (args.length != 1)
+          {
+            printUsage();
+            System.exit(-1);
+          }
+
+        String pathName = args[0];
+        Path startingDir = Paths.get(pathName);
+
+        PrintDirectoryStructure pds = new PrintDirectoryStructure();
+        Files.walkFileTree(startingDir, pds);
+
+        System.out.println(pathName + ": " + cdsv.getDiskSpace() + " bytes");
+      }
+
+
+    public long getDiskSpace()
+      {
+        return diskSpace;
+      }
+
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes bfAttrs)
+      {
+        System.out.println("File " + file.getFileName() + " has size " + bfAttrs.size());
+        diskSpace = diskSpace + bfAttrs.size();
+        return CONTINUE;
+      }
+
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes bfAttrs)
+      {
+        System.out.println("Directory " + dir.getFileName() + " has size " + bfAttrs.size());
+        diskSpace = diskSpace + bfAttrs.size();
+        return CONTINUE;
+      }
+
+
+    // Use inherited implementation for method visitFileFailed()
+
+
+    private static void printUsage()
+      {
+        System.out.println("Usage: java edu.citadel.csci603.util.ComputeDiskSpaceVisitor(<path>)");
+        System.out.println("       where <path> is the path of a file or directory");
+        System.out.println();
+      }
+  }
