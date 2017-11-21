ac30b2c6aa6a298b5d9cc06fbc26615fee3180d7
pabaier <pabaier@gmail.com>
2017-11-21 17:29:13 -0500

    wierd formatting

--- 10 - Print Directory Structure Using Visitor/edu/citadel/csci603/util/PrintDirectoryStructure.java
+++ 10 - Print Directory Structure Using Visitor/edu/citadel/csci603/util/PrintDirectoryStructure.java
@@ -11,16 +11,21 @@ import static java.nio.file.FileVisitResult.*;
  */
 public class PrintDirectoryStructure extends SimpleFileVisitor<Path>
   {
+
+    private static int indentLevel = 0;
+    private static final String dirSign = "+";
+    private static final String fileSign = "-";
+
     /**
-     * Prints the structure for the file whose path name is given in arg[0].
-     */
+    * Prints the structure for the file whose path name is given in arg[0].
+    */
     public static void main(String[] args) throws IOException
-      {
+    {
         if (args.length != 1)
-          {
+        {
             printUsage();
             System.exit(-1);
-          }
+        }
 
         String pathName = args[0];
         Path startingDir = Paths.get(pathName);
@@ -28,41 +33,50 @@ public class PrintDirectoryStructure extends SimpleFileVisitor<Path>
         PrintDirectoryStructure pds = new PrintDirectoryStructure();
         Files.walkFileTree(startingDir, pds);
 
-        System.out.println(pathName + ": " + cdsv.getDiskSpace() + " bytes");
-      }
-
-
-    public long getDiskSpace()
-      {
-        return diskSpace;
-      }
-
+        // System.out.println(pathName + ": " + cdsv.getDiskSpace() + " bytes");
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes bfAttrs)
-      {
-        System.out.println("File " + file.getFileName() + " has size " + bfAttrs.size());
-        diskSpace = diskSpace + bfAttrs.size();
-        return CONTINUE;
-      }
+    {
+        System.out.print(getIndent());
+        if(bfAttrs.isDirectory())
+            System.out.print(dirSign);
+        else
+            System.out.println(fileSign);
+        System.out.print(file.getFileName());
 
+        return CONTINUE;
+    }
+
+
+    // @Override
+    // public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes bfAttrs)
+    // {
+
+        
+    // }
 
     @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes bfAttrs)
-      {
-        System.out.println("Directory " + dir.getFileName() + " has size " + bfAttrs.size());
-        diskSpace = diskSpace + bfAttrs.size();
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        indentLevel ++;
         return CONTINUE;
-      }
+    }
 
 
     // Use inherited implementation for method visitFileFailed()
 
+    private static String getIndent() {
+        StringBuilder s = new StringBuilder();
+        for(int i = 0; i < indentLevel; i++)
+            s.append("\t");
+        return s.toString();
+    }
 
     private static void printUsage()
-      {
+    {
         System.out.println("Usage: java edu.citadel.csci603.util.ComputeDiskSpaceVisitor(<path>)");
         System.out.println("       where <path> is the path of a file or directory");
         System.out.println();
-      }
+    }
   }
