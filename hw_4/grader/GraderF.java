d9a992ad15453cc5b6187c68820f6d91af7e2665
pabaier <pabaier@gmail.com>
2017-11-26 13:34:09 -0500

    starting appointment list tests

--- hw_4/grader/GraderF.java
+++ hw_4/grader/GraderF.java
@@ -1,16 +1,66 @@
 import edu.cofc.grader.*;
+import java.io.File;
+import org.apache.commons.io.*;
 
 public class GraderF {
      public static void main(String[] args){
 
-        TestOutline calendarDateTests = new TestOutline("Calendar Date Tests");
-        TestOutline appointmentTests = new TestOutline("Appointment Tests");
-        TestOutline appointmentListTests = new TestOutline("AppointmentList Tests");
+         C.colors(true);
+
+        TestOutline calendarDateTests = new TestOutline(C.UNDERLINE + "CalendarDate" + C.RESET);
+        TestOutline appointmentTests = new TestOutline(C.UNDERLINE + "Appointment" + C.RESET);
+        TestOutline appointmentListTests = new TestOutline(C.UNDERLINE + "AppointmentList" + C.RESET);
         TestOutline hw4Tests = new TestOutline("HW 4 Tests");
 
+        // calendarDate
         SingleTests.IsAValidDate cdtDate = new SingleTests.IsAValidDate();
-        cdtDate.setName("Is A Valid Date Test");
-        calendarDateTests.addTest(cdtDate);
+        cdtDate.setName("Is A Valid Date");
+
+        calendarDateTests.add(cdtDate);
+
         calendarDateTests.run();
+ 
+        // appointment
+        SingleTests.AppointmentConstructor aptConst = new SingleTests.AppointmentConstructor();
+        aptConst.setName("Appointment Constructor");
+        
+        SingleTests.GetEmployee getEmployee = new SingleTests.GetEmployee();
+        getEmployee.setName("getEmployee()");
+        
+        SingleTests.GetDate getDate = new SingleTests.GetDate();
+        getDate.setName("getDate()");
+
+        SingleTests.AppointmentToString aptToString = new SingleTests.AppointmentToString();
+        aptToString.setName("toString()");
+        
+        appointmentTests.add(aptConst);
+        appointmentTests.add(getEmployee);
+        appointmentTests.add(getDate);
+        appointmentTests.add(aptToString);
+
+        appointmentTests.run();
+
+        fetchKey("Appointment");
+
+        // appointment list
+        SingleTests.AppointmentListConstructor aptListConst = new SingleTests.AppointmentListConstructor();
+        aptListConst.setName("AppointmentList Constructor");
+
+        appointmentListTests.add(aptListConst);
+
+        appointmentListTests.run();
+
      }
+
+    // Removes the student's class and replaces it with the key class
+    private static void fetchKey(String className) {
+        File old = new File("D:\\School\\221\\hw_4\\grader\\" + className + ".class");
+        File newFile = new File("D:\\School\\221\\hw_4\\HW4\\" + className + ".class");
+        File newDir = new File("D:\\School\\221\\hw_4\\grader");
+        FileUtils.deleteQuietly(old);
+        try {
+            FileUtils.copyFileToDirectory(newFile, newDir);
+        }
+        catch(Exception e){}
+    }
 }
--- hw_4/grader/SingleTests.java
+++ hw_4/grader/SingleTests.java
@@ -2,11 +2,12 @@ import edu.cofc.grader.*;
 
 public class SingleTests {
     
+    // calendar
     public static class IsAValidDate extends SingleTest {
         public void exec() {
-            setTotalPoints(15);
-            int full = 3;
-            int half = 2;
+            setTotalPoints(10);
+            int full = 2;
+            int half = 1;
             // 0 - month > 12
             // 1 - month < 1
             // 2 - day > 31
@@ -29,20 +30,144 @@ public class SingleTests {
                                     new CalendarDate(2016, 2, 29),
                                     new CalendarDate(2015, 2, 29)};
                 for(int i = 0; i < key.length; i++) {
-                    System.out.print(getIndent() + "   " + key[i] + ": " + student[i].isAValidDate());
+                    System.out.print(indent() + key[i] + ": " + student[i].isAValidDate());
                     if(key[i].isAValidDate() == student[i].isAValidDate()) {
-                        System.out.println(" - Correct - " + full + "/" + full);
+                        System.out.println(C.CORRECT + " - Correct - " + full + "/" + full + C.RESET);
                         addPoints(full);
                     }
                     else {
-                        System.out.println(" - Incorrect - " + half + "/" + full);
+                        System.out.println(C.PARTCORRECT + " - Incorrect - " + half + "/" + full + C.RESET);
                         addPoints(half);
                     }
                 }
             }
             catch(Exception | NoClassDefFoundError e) {
-                System.out.println(getIndent() + "Error running test");
-                System.out.println(getIndent() + e);
+                System.out.println(getIndent() + C.INCORRECT + "Error running test");
+                System.out.println(getIndent() + e + C.RESET);
+            }
+        }
+    }
+
+    // appointment
+    public static class AppointmentConstructor extends SingleTest {
+        public void exec() {
+            setTotalPoints(1);
+            int full = 1;
+            int half = 0;
+            try {
+                Appointment a = new Appointment(new CalendarDate(2016, 2, 2), new Employee("John"));
+                System.out.println(indent() + C.CORRECT + "Correct - " + full + "/" + full + C.RESET);
+                addPoints(full);
+            }
+            catch(Exception e) {
+                System.out.println(indent() + C.INCORRECT + e);
+                System.out.println(indent() + "Error running test - " + getPointsEarned() + "/" + full + C.RESET);
+            }
+        }
+    }
+
+    public static class GetEmployee extends SingleTest {
+        public void exec() {
+            setTotalPoints(3);
+            int full = 3;
+            int half = 2;
+            AppointmentKey key = new AppointmentKey(new CalendarDate(2016, 2, 2),  new Employee("John"));
+            try {
+                Appointment student = new Appointment(key.getDate(), key.getEmployee());
+                System.out.print(indent() + key.getEmployee() + " | " + student.getEmployee());
+                if(student.getEmployee() == key.getEmployee()) {
+                    System.out.println(C.CORRECT + " - Correct - " + full + "/" + full + C.RESET);
+                    addPoints(full);
+                }
+                else {
+                    System.out.println(C.PARTCORRECT + " - Incorrect - " + half + "/" + full + C.RESET);
+                    addPoints(half);
+                }
+            }
+            catch(Exception e) {
+                System.out.println(indent() + C.INCORRECT + e);
+                System.out.println(indent() + "Error running test - " + getPointsEarned() + "/" + full + C.RESET);
+            }
+        }
+    }
+
+    public static class GetDate extends SingleTest {
+        public void exec() {
+            setTotalPoints(3);
+            int full = 3;
+            int half = 2;
+            AppointmentKey key = new AppointmentKey(new CalendarDate(2016, 2, 2),  new Employee("John"));
+            try {
+                Appointment student = new Appointment(key.getDate(), key.getEmployee());
+                System.out.print(indent()  + key.getDate() + " | " + student.getDate());
+                if(student.getDate() == key.getDate()) {
+                    System.out.println(C.CORRECT + " - Correct - " + full + "/" + full + C.RESET);
+                    addPoints(full);
+                }
+                else {
+                    System.out.println(C.PARTCORRECT + " - Incorrect - " + half + "/" + full + C.RESET);
+                    addPoints(half);                
+                }
+            }
+            catch(Exception e) {
+                System.out.println(indent() + C.INCORRECT + e);
+                System.out.println(indent() + "Error running test - " + getPointsEarned() + "/" + full + C.RESET);
+            }
+        }
+    }
+
+    public static class AppointmentToString extends SingleTest {
+        public void exec() {
+            setTotalPoints(3);
+            int full = 3;
+            int half = 2;
+            
+            AppointmentKey key = new AppointmentKey(new CalendarDate(1762, 9, 12),  new Employee("Yekaterina Alekseyevna"));
+            try {
+                Appointment student = new Appointment(key.getDate(), key.getEmployee());
+                String studentAnswer = student.toString().toUpperCase();
+                System.out.print(indent() + key + " | " + student);
+                if(studentAnswer.contains(key.getDate().toString().toUpperCase()) &&
+                studentAnswer.contains(key.getEmployee().getName().toUpperCase())) {
+                    System.out.println(C.CORRECT + " - Correct - " + full + "/" + full + C.RESET);
+                    addPoints(full);
+                }
+                else if(studentAnswer.contains(key.getDate().toString().toUpperCase())) {
+                    System.out.println(C.PARTCORRECT + " - Date Correct - " + half + "/" + full + C.RESET);
+                    addPoints(half);
+                }
+                else if(studentAnswer.contains(key.getEmployee().getName().toUpperCase())) {
+                    System.out.println(C.PARTCORRECT + " - Name Correct - " + half + "/" + full + C.RESET);
+                    addPoints(half);
+                }
+                else {
+                    System.out.println(C.PARTCORRECT + " - Incorrect - " + (half - 1) + "/" + full + C.RESET);
+                    addPoints(half - 1);                
+                }
+            }
+            catch(Exception e) {
+                System.out.println(indent() + C.INCORRECT + e);
+                System.out.println(indent() + "Error running test - " + getPointsEarned() + "/" + full + C.RESET);
+            }
+        }
+    }
+
+    //appointmentList
+    public static class AppointmentListConstructor extends SingleTest {
+        public void exec() {
+            setTotalPoints(1);
+            int full = 1;
+            int half = 0;
+
+            try {
+                AppointmentList a = new AppointmentList();
+                System.out.println(indent() + C.CORRECT + "Correct - " + full + "/" + full + C.RESET);
+                addPoints(full);
+            }
+
+            catch(Exception e) {
+                System.out.println(indent() + C.INCORRECT + e);
+                System.out.println(indent() + "Error running test - " + getPointsEarned() + "/" + full + C.RESET);
             }
         }
     }
