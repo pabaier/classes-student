4d75fc835d0fab0c9cd140e3393871c2c16813d2
pabaier <pabaier@gmail.com>
2017-11-26 21:56:14 -0500

    starting to grade

--- hw_4/grader/GraderF.java
+++ hw_4/grader/GraderF.java
@@ -7,7 +7,9 @@ import java.util.ArrayList;
 public class GraderF {
      public static void main(String[] args){
 
-         C.colors(true);
+        C.colors(true);
+        C.setTitlesArray(new String[]{C.CYAN, C.BLUE, C.MAGENTA});
+        C.setResultsArray(new String[]{C.MAGENTA, C.BOLD+C.BACKGROUND_BLACK+C.YELLOW, C.CYAN});
 
         TestOutline calendarDateTests = new TestOutline(C.UNDERLINE + "CalendarDate" + C.RESET);
         TestOutline appointmentTests = new TestOutline(C.UNDERLINE + "Appointment" + C.RESET);
@@ -81,9 +83,9 @@ public class GraderF {
 
     // Removes the student's class and replaces it with the key class
     private static void fetchKey(String className) {
-        File old = new File("D:\\School\\221\\hw_4\\grader\\" + className + ".class");
+        File old = new File("D:\\School\\221\\hw_4\\grader\\classes" + className + ".class");
         File newFile = new File("D:\\School\\221\\hw_4\\HW4\\" + className + ".class");
-        File newDir = new File("D:\\School\\221\\hw_4\\grader");
+        File newDir = new File("D:\\School\\221\\hw_4\\grader\\classes");
         FileUtils.deleteQuietly(old);
         try {
             FileUtils.copyFileToDirectory(newFile, newDir);
