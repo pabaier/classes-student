d125cd53cb9a93196204a7ed9e5af8762746dc4f
= <baierpa@g.cofc.edu>
2018-03-13 12:25:45 -0400

    separating main from client

--- 2/Eight Puzzle Solution/Main.java
+++ 2/Eight Puzzle Solution/Main.java
@@ -2,12 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-
-		
-		EightPuzzle game = new EightPuzzle(new PriorityQueueHeap(), new int[]{0,1,2,3,4,5,6,7,8});
-		// int[][] myArray = EightPuzzle.generateAllPossibleStates();
-		// EightPuzzle.print2DArray(myArray);
-		// System.out.println();
+		int[] is = new int[]{0,1,2,3,4,5,6,7,8};
+		Client c = new Client(is);
 	}
 
 }
