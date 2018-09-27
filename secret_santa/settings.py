e67cc7f59c12b26667258998e1284037b156539b
baierpa <baierpa@g.cofc.edu>
2018-09-27 19:21:05 -0400

    use config file for db connection

--- secret_santa/settings.py
+++ secret_santa/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
+import sys
+
+with open(os.path.join(sys.path[0], 'secret_santa/config.json'), 'r') as f:
+    config = json.load(f)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +79,9 @@ WSGI_APPLICATION = 'secret_santa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+db = config['db']
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': db['heroku'] 
 }
 
 
