03e51f14621b36ce79a9e11caf3e7d8c0de1eee4
baierpa <baierpa@g.cofc.edu>
2018-10-10 15:07:32 -0400

    rename santa and remove some stragglers

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import SignUpView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('', include('django.contrib.auth.urls')),
+    path('signup/', SignUpView.as_view(), name='signup')
 ]
