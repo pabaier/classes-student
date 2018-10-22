8464d0dbc516f1ef6ca1cab3ccf48e89650aefeb
baierpa <baierpa@g.cofc.edu>
2018-10-22 08:51:17 -0400

    add navbar

--- secret_santa/settings.py
+++ secret_santa/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/main/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main')]
+STATIC_URL = '/main/'
 
 LOGIN_REDIRECT_URL = 'home'
 
