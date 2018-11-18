312c4cc45ec945a56ebd7cc2a8e6a98fffa41108
baierpa <baierpa@g.cofc.edu>
2018-11-17 23:56:21 -0500

    add modal with response data to send button

--- comms/views.py
+++ comms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 from twilio.rest import Client
 from members.models import Pairings, User_By_Group as userGroups
@@ -13,41 +15,62 @@ def sendText(userName, pairingName, groupName, userPhoneNumber):
 
   client = Client(tConfig['account_sid'], tConfig['auth_token'])
   txt = f'Hello {userName}!\nYour Secret Santa pairing for group {groupName} is {pairingName}!'
-  
+
   try:
-      message = client.messages.create(
-          to    = f'{userPhoneNumber}',
-          from_ = tConfig['number'],
-          body  = txt
-      )
+    message = client.messages.create(
+      to    = f'{userPhoneNumber}',
+      from_ = tConfig['number'],
+      body  = txt
+    )
+    result = {'success': {'userName': userName}}
   except Exception as err:
-      message = err.__dict__['msg']
-  context = {"message": message}
+    message = err.__dict__['msg']
+    result = {'failure': {'userName': userName}}
+
+  return {'result': result}
 
 def sendEmail(userName, pairingName, groupName, userEmail):
   subject =  f'Secret Santa Pairing For Group {groupName}'
   message = f'Hello {userName}!\nYour Secret Santa pairing for group {groupName} is {pairingName}!'
   toEmail = [userEmail]
-  send_mail(subject, message, settings.EMAIL_HOST_USER, toEmail, fail_silently=False,)
+  try:
+    send_mail(subject, message, settings.EMAIL_HOST_USER, toEmail, fail_silently=False,)
+    result = {'success': {'userName': userName}}
+  except Exception as err:
+    message = err.__dict__['msg']
+    result = {'failure': {'userName': userName, 'error': message}}
+  return result
 
+@csrf_exempt
 def send(request):
   groupId = request.POST['groupId']
-  pairings = Pairings.objects.filter(groupID=groupId)
-  groupName = myGroups.objects.filter(id=groupId)[0].group_name
-  d = {}
-  for pairs in pairings:
-    user = pairs.member_1ID
-    pair = pairs.member_2ID
-    if(user.phone and user.phone != ''):
-      d[user.id] = {'username':user.username, 'phone':user.phone, 'pairing':pair.username}
-      sendText(user.username, pair.username, groupName, user.phone)
-    else:
-      d[user.id] = {'username':user.username, 'email':user.email, 'pairing':pair.username}
-      sendEmail(user.username, pair.username, groupName, user.email)
-
-  context = {'data': d}
-
-  return render(request, 'comms/sent.html', context)
+  # pairings = Pairings.objects.filter(groupID=groupId)
+  # groupName = myGroups.objects.filter(id=groupId)[0].group_name
+  # d = {}
+  # results = {'success': [], 'failure': []}
+  # for pairs in pairings:
+  #   user = pairs.member_1ID
+  #   pair = pairs.member_2ID
+  #   if(user.phone and user.phone != ''):
+  #     d[user.id] = {'username':user.username, 'phone':user.phone, 'pairing':pair.username}
+  #     result = sendText(user.username, pair.username, groupName, user.phone)
+  #   else:
+  #     d[user.id] = {'username':user.username, 'email':user.email, 'pairing':pair.username}
+  #     result = sendEmail(user.username, pair.username, groupName, user.email)
+  # if 'success' in result:
+  #   results['success'].append(result['success'])
+  # else:
+  #   results['failture'].append(result['failure'])
+  results = {
+    'success': [{'userName': 'user1'}, {'userName': 'user3'}], 
+    'failure': [{'userName': 'user2', 'error': 'This messed up'}, {'userName': 'user4', 'error': 'you messed up'}]
+  }
+  results = {
+    'success': [{'userName': 'user1'}, {'userName': 'user3'}], 
+    'failure': []
+  }
+  output = {k:v for (k,v) in results.items() if len(results[k]) > 0}
+  return JsonResponse(output)
 
 def test(request):
   return render(request, 'comms/testPost.html')
