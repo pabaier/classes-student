e3a6ac47016f913d4abbe912e2ed71f8713db17d
baierpa <baierpa@g.cofc.edu>
2018-11-18 21:44:07 -0500

    change responses for send group and send user

--- comms/views.py
+++ comms/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 from twilio.rest import Client
 from members.models import Pairings, User_By_Group as userGroups
@@ -25,7 +24,7 @@ def sendText(userName, pairingName, groupName, userPhoneNumber):
     result = {'success': {'userName': userName}}
   except Exception as err:
     message = err.__dict__['msg']
-    result = {'failure': {'userName': userName}}
+    result = {'failure': {'userName': userName, 'error': message}}
   return result
 
 def sendEmail(userName, pairingName, groupName, userEmail):
@@ -42,51 +41,78 @@ def sendEmail(userName, pairingName, groupName, userEmail):
 
 def sendGroup(request, groupId):
   if (not userGroups.objects.filter(member_1ID=request.user.id, group_ID=groupId).exists()):
-    return JsonResponse({'failure': [{'message': 'You are not part of the requested group'}]})
-  # pairings = Pairings.objects.filter(groupID=groupId)
-  # groupName = myGroups.objects.filter(id=groupId)[0].group_name
-  # results = {'success': [], 'failure': []}
-  # for pairs in pairings:
-  #   user = pairs.member_1ID
-  #   pair = pairs.member_2ID
-  #   if(user.phone and user.phone != ''):
-  #     result = sendText(user.username, pair.username, groupName, user.phone)
-  #   else:
-  #     result = sendEmail(user.username, pair.username, groupName, user.email)
-  # if 'success' in result:
-  #   results['success'].append(result['success'])
-  # else:
-  #   results['failure'].append(result['failure'])
-  results = {
-    'success': [{'message': 'user1'}, {'message': 'user3'}], 
-    'failure': [{'message': 'user2'}, {'message': 'user4'}]
-  }
-  results = {
-    'success': [{'message': 'user1'}, {'message': 'user3'}], 
-    'failure': []
-  }
+    return JsonResponse({'failure': [{
+        'userName': request.user.username,
+        'userId': request.user.id,
+        'groupName': '',
+        'groupId': groupId,
+      'message': 'You are not part of the requested group'
+    }]})
+  pairings = Pairings.objects.filter(groupID=groupId)
+  groupName = myGroups.objects.filter(id=groupId)[0].group_name
+  results = {'success': [], 'failure': []}
+  for pairs in pairings:
+    user = pairs.member_1ID
+    pair = pairs.member_2ID
+    if(user.phone and user.phone != ''):
+      result = sendText(user.username, pair.username, groupName, user.phone)
+    else:
+      result = sendEmail(user.username, pair.username, groupName, user.email)
+    if 'success' in result:
+      results['success'].append({
+        'userName': user.username,
+        'userId': user.id,
+        'groupName': groupName,
+        'groupId': groupId,
+        'message': f"Success contacting {result['success']['userName']}",
+        })
+    else:
+      results['failure'].append({
+        'userName': user.username,
+        'userId': user.id,
+        'groupName': groupName,
+        'groupId': groupId,
+        'message': f"Failed contacting {result['failure']['userName']} - {result['failure']['error']}",
+        })
+  
   output = {k:v for (k,v) in results.items() if len(results[k]) > 0}
   return JsonResponse(output)
 
 def sendUser(request, groupId, userId):
   if (not userGroups.objects.filter(member_1ID=request.user.id, group_ID=groupId).exists()):
-    return JsonResponse({'failure': [{'message': 'You are not part of the requested group'}]})
+    return JsonResponse({'failure': {
+      'userName': request.user.username,
+      'userId': request.user.id,
+      'groupName': '',
+      'groupId': groupId,
+      'message': 'You are not part of the requested group'
+      }})
   userPairing = Pairings.objects.filter(groupID=groupId, member_1ID=userId)[0]
   groupName = myGroups.objects.filter(id=groupId)[0].group_name
-  results = {'success': [], 'failure': []}
+  results = {}
   user = userPairing.member_1ID
   pair = userPairing.member_2ID
   if(user.phone and user.phone != ''):
     result = sendText(user.username, pair.username, groupName, user.phone)
   else:
     result = sendEmail(user.username, pair.username, groupName, user.email)
-  print(result)
   if ('success' in result):
-    results['success'].append(result['success'])
+    results['success'] = {
+      'userName': user.username,
+      'userId': userId,
+      'groupName': groupName,
+      'groupId': groupId,
+      'message': f"Success contacting {result['success']['userName']}"
+    }
   else:
-    results['failure'].append(result['failure'])
-  output = {k:v for (k,v) in results.items() if len(results[k]) > 0}
-  return JsonResponse(output)
+    results['failure'] = {
+      'userName': user.username,
+      'userId': userId,
+      'groupName': groupName,
+      'groupId': groupId,
+      'message': f"Failed contacting {result['failure']['userName']} - {result['failure']['error']}"
+    }
+  return JsonResponse(results)
 
 def testGroupSend(request):
   return render(request, 'comms/testGroupPairs.html')
