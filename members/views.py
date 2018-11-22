6a037d9aeb375fc5d530737d9aa602e3561b44e8
baierpa <baierpa@g.cofc.edu>
2018-11-21 20:14:43 -0500

    update the record in the model

--- members/views.py
+++ members/views.py
@@ -5,7 +5,9 @@ from django.urls import reverse_lazy
 from django.views import generic
 from members.models import Members
 from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 import json
+import re
 
 class MembersView(generic.CreateView):
     form_class = MembersCreationForm
@@ -15,14 +17,20 @@ class MembersView(generic.CreateView):
 def profile(request):
   if request.user.id:
     result = Members.objects.get(id=request.user.id)
-    for e in Members._meta.get_fields():
-      print(e)
+    resultDict = model_to_dict(result)
+    for key, value in resultDict.items():
+      if value is None:
+        resultDict[key] = ""
     result = {
-      'username': result.username,
-      'phone': result.phone,
-      'firstname': result.first_name,
-      'lastname': result.last_name,
-      'email': result.email
+      'username': resultDict['username'],
+      'phone': resultDict['phone'],
+      'firstname': resultDict['first_name'],
+      'lastname': resultDict['last_name'],
+      'email': resultDict['email'],
+      'address': resultDict['address'],
+      'city': resultDict['city'],
+      'state': resultDict['state'],
+      'zip': resultDict['zip_code']
     }
   return render(request, 'profile.html', {'result': result})
 
@@ -36,15 +44,23 @@ def login(request):
 def update(request, userId):
   body_unicode = request.body.decode('utf-8')
   body = json.loads(body_unicode)
+  for key in body.keys():
+    if body[key] == '':
+      body[key] = None
   try:
     Members.objects.filter(id=userId).update(
       email=body['email'], 
       phone=body['phone'],
       username=body['username'],
       first_name=body['firstname'],
-      last_name=body['lastname']
+      last_name=body['lastname'],
+      address=body['address'],
+      city=body['city'],
+      state=body['state'],
+      zip_code=body['zip']
       )
     return JsonResponse({"success": True})
-  except:
-    return JsonResponse({"success": False})
+  except Exception as e:
+    message = re.sub(r'members_members\.','',str(e))
+    return JsonResponse({"success": False, "message": message})
   
