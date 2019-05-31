81466d77101a77a089f99c7c3efa95ac7d9c7b46
pbaier <pbaier@boomtownroi.com>
2019-05-31 19:14:25 -0400

    sign and verify data

--- client/client.py
+++ client/client.py
@@ -1,23 +1,49 @@
 from os import chmod
-from Crypto.PublicKey import RSA
 import base64
 import os
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_PSS
 
 def main():
 	# createKeys()
 	# deleteKeys()
-	# print(getKey('private.key'))
 	pubKey = getKey('public.key')
 	privateKey = getKey('private.key')
 	a = encrypt(pubKey, 'you are my love!')
 	b = decrypt(privateKey, a)
-	print(a)
-	print(b)
+	signature = sign(privateKey, a)
+	verification = verify(pubKey, signature, a)
+	print(verification)
 
+# sign the encrypted data with the private key
+# first SHA the data, then create the signer from the private key
+# then sign the SHA with the signer
+def sign(privateKey, data):
+	h = SHA.new()
+	h.update(data)
+	signer = PKCS1_PSS.new(privateKey)
+	signature = signer.sign(h)
+	return signature
+
+# verify the encrypted data
+# first SHA the data, then create the verifier from the public key
+# then verify the SHA'd data and signature with the verifier
+def verify(publicKey, signature, data):
+	h = SHA.new()
+	h.update(data)
+	verifier = PKCS1_PSS.new(publicKey)
+	isVerified = verifier.verify(h, signature)
+	return isVerified 
+
+# decrypt to string using private key
+# first decode the base64, then decrypt with private key
 def decrypt(key, cipher):
 	a = base64.b64decode(cipher)
 	return key.decrypt(a).decode("utf-8")
 
+# encrypt to base64 encoded bytes using public key
+# first encrypt using public key, then base64 encode
 def encrypt(key, text):
 	encryptedBytes = key.encrypt(str.encode(text), 32)[0]
 	return base64.b64encode(encryptedBytes)
