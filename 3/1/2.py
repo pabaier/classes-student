6be377cea9e9595baf3bc24774e59afe04a61284
pabaier <baierpa@g.cofc.edu>
2019-09-13 09:06:29 -0400

    add main method

--- 3/1/2.py
+++ 3/1/2.py
@@ -28,33 +28,38 @@ ENGLISH_LETTER_FREQUENCIES = {
 	'z':0.002 
 }
 
-message = "TSMVM MPPCW CZUGX HPECP RFAUE IOBQW PPIMS FXIPC TSQPK SZNUL OPACR DDPKT SLVFW ELTKR GHIZS FNIDF ARMUE NOSKR GDIPH WSGVL EDMCM SMWKP IYOJS TLVFA HPBJI RAQIW HLDGA IYOUX".lower().replace(" ", "")
+def main():
+	message = "TSMVM MPPCW CZUGX HPECP RFAUE IOBQW PPIMS FXIPC TSQPK SZNUL OPACR DDPKT SLVFW ELTKR GHIZS FNIDF ARMUE NOSKR GDIPH WSGVL EDMCM SMWKP IYOJS TLVFA HPBJI RAQIW HLDGA IYOUX".lower().replace(" ", "")
 
-# get frequency of each letter in each position (positions are 1-5 because we know the key length is 5)
-letterFrequencies = {0: {}, 1:{}, 2:{}, 3:{}, 4:{}}
-for i in range(0,len(message)):
-	letterIndex = i%5
-	letter = message[i]
-	if letter in letterFrequencies[letterIndex]:
-		letterFrequencies[letterIndex][letter] += 1
-	else:
-		letterFrequencies[letterIndex][letter] = 1
+	# get frequency of each letter in each position (positions are 1-5 because we know the key length is 5)
+	letterFrequencies = {0: {}, 1:{}, 2:{}, 3:{}, 4:{}}
+	for i in range(0,len(message)):
+		letterIndex = i%5
+		letter = message[i]
+		if letter in letterFrequencies[letterIndex]:
+			letterFrequencies[letterIndex][letter] += 1
+		else:
+			letterFrequencies[letterIndex][letter] = 1
 
-letterFrequencyPercentages = copy.deepcopy(letterFrequencies)
+	letterFrequencyPercentages = copy.deepcopy(letterFrequencies)
 
-# get frequency percentages
-total = len(message)/5 # 28
-for entry in letterFrequencies:
-	for letter in letterFrequencies[entry]:
-		letterFrequencyPercentages[entry][letter] /= total 
+	# get frequency percentages
+	total = len(message)/5 # 28
+	for entry in letterFrequencies:
+		for letter in letterFrequencies[entry]:
+			letterFrequencyPercentages[entry][letter] /= total 
 
-# cycle through percentages to see which shift is most likely
-# for i in range(0, 25): # i is the shift
+	# cycle through percentages to see which shift is most likely
+	# for i in range(0, 25): # i is the shift
 
-a = list(letterFrequencies[0].keys())
-letterFrequencies[0].pop(a[0])
+	a = list(letterFrequencies[0].keys())
+	letterFrequencies[0].pop(a[0])
 
-for row in letterFrequencies:
-	print(letterFrequencies[row])
-for row in letterFrequencyPercentages:
-	print(letterFrequencyPercentages[row])
+	printDict(letterFrequencies)
+
+def printDict(dicti):
+	for row in dicti:
+		print(dicti[row])
+
+if __name__ == "__main__":
+	main()
