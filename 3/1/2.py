669a1db11814821c0e00c2af7d9e6514e2a6de0e
pabaier <baierpa@g.cofc.edu>
2019-09-13 08:59:08 -0400

    rename variables

--- 3/1/2.py
+++ 3/1/2.py
@@ -31,29 +31,30 @@ ENGLISH_LETTER_FREQUENCIES = {
 message = "TSMVM MPPCW CZUGX HPECP RFAUE IOBQW PPIMS FXIPC TSQPK SZNUL OPACR DDPKT SLVFW ELTKR GHIZS FNIDF ARMUE NOSKR GDIPH WSGVL EDMCM SMWKP IYOJS TLVFA HPBJI RAQIW HLDGA IYOUX".lower().replace(" ", "")
 
 # get frequency of each letter in each position (positions are 1-5 because we know the key length is 5)
-letters = {0: {}, 1:{}, 2:{}, 3:{}, 4:{}}
+letterFrequencies = {0: {}, 1:{}, 2:{}, 3:{}, 4:{}}
 for i in range(0,len(message)):
 	letterIndex = i%5
 	letter = message[i]
-	if letter in letters[letterIndex]:
-		letters[letterIndex][letter] += 1
+	if letter in letterFrequencies[letterIndex]:
+		letterFrequencies[letterIndex][letter] += 1
 	else:
-		letters[letterIndex][letter] = 1
+		letterFrequencies[letterIndex][letter] = 1
 
-letterFrequencyPercentages = copy.deepcopy(letters)
+letterFrequencyPercentages = copy.deepcopy(letterFrequencies)
+
 # get frequency percentages
 total = len(message)/5 # 28
-for entry in letters:
-	for letter in letters[entry]:
+for entry in letterFrequencies:
+	for letter in letterFrequencies[entry]:
 		letterFrequencyPercentages[entry][letter] /= total 
 
 # cycle through percentages to see which shift is most likely
 # for i in range(0, 25): # i is the shift
 
-a = list(letters[0].keys())
-letters[0].pop(a[0])
+a = list(letterFrequencies[0].keys())
+letterFrequencies[0].pop(a[0])
 
-for row in letters:
-	print(letters[row])
 for row in letterFrequencies:
 	print(letterFrequencies[row])
+for row in letterFrequencyPercentages:
+	print(letterFrequencyPercentages[row])
