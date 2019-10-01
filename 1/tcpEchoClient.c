fa12adc5bea379b01f4a8f3d8944fcbd9b2f89ce
pabaier <baierpa@g.cofc.edu>
2019-10-01 18:26:57 -0400

    finish outputs

--- 1/tcpEchoClient.c
+++ 1/tcpEchoClient.c
@@ -5,162 +5,160 @@
 #include<netdb.h>
 #include<string.h>
 #include<stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 // #define SERVER_PORT 7777
 #define MAX_LINE 256
 #define MAXNAME 256
 
 /* structure of the packet */
-struct packet{
-	short type;
-	char uName[MAXNAME];
-	char mName[MAXNAME];
-	char data[MAXNAME];
+struct packet {
+    short type;
+    char uName[MAXNAME];
+    char mName[MAXNAME];
+    char data[MAXNAME];
 };
 
-int main(int argc, char* argv[])
-{
-	struct hostent *hp;
-	struct sockaddr_in sin;
-	char *host, *userName;
-	char computerName[MAXNAME];
-	char buf[MAX_LINE];
-	int s;
-	struct packet packet_reg;
-	struct packet packet_reg_confirm;
-	struct packet packet_chat;
-	struct packet packet_chat_response;
-	short SERVER_PORT = 7777;
+int main(int argc, char *argv[]) {
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host, *userName;
+    char computerName[MAXNAME];
+    char buf[MAX_LINE];
+    int s;
+    struct packet packet_reg;
+    struct packet packet_reg_confirm;
+    struct packet packet_chat;
+    struct packet packet_chat_response;
+    short SERVER_PORT = 7777;
 
-	/*
-		The following grabs the command line arguments.
-		Besides the name of the program,
-		if there are 2 arguments, the host will be the first one,
-		and the user name will be the second one.
-		if there is only 1 argument, the host will be 'localhost'
-		and the username will be the argument.
-		if there are no arguments, the program will exit.
-	*/
-	if(argc == 3){
-		host = argv[1];
-		userName = argv[2];
-		// SERVER_PORT = atoi(argv[2]);
-	}
-	else if(argc == 2){
-		host = "localhost";
-		userName = argv[1];
-	}
-	else{
-		fprintf(stderr, "usage:newclient username\n");
-		exit(1);
-	}
+    /*
+        The following grabs the command line arguments.
+        Besides the name of the program,
+        if there are 2 arguments, the host will be the first one,
+        and the user name will be the second one.
+        if there is only 1 argument, the host will be 'localhost'
+        and the username will be the argument.
+        if there are no arguments, the program will exit.
+    */
+    if (argc == 3) {
+        host = argv[1];
+        userName = argv[2];
+        // SERVER_PORT = atoi(argv[2]);
+    } else if (argc == 2) {
+        host = "localhost";
+        userName = argv[1];
+    } else {
+        fprintf(stderr, "usage:newclient username\n");
+        exit(1);
+    }
 
-	/* translate host name into peer's IP address */
-	hp = gethostbyname(host);
-	if(!hp){
-		fprintf(stderr, "unkown host: %s\n", host);
-		exit(1);
-	}
-	gethostname(computerName, sizeof(computerName));
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "unkown host: %s\n", host);
+        exit(1);
+    }
+    gethostname(computerName, sizeof(computerName));
 
-	/* active open */
-	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-		perror("tcpclient: socket");
-		exit(1);
-	}
+    /* active open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("tcpclient: socket");
+        exit(1);
+    }
 
-	/* build address data structure */
-	bzero((char*)&sin, sizeof(sin));
-	sin.sin_family = AF_INET;
-	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
-	sin.sin_port = htons(SERVER_PORT);
+    /* build address data structure */
+    bzero((char *) &sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *) &sin.sin_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
 
-	if(connect(s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
-		perror("tcpclient: connect");
-		close(s);
-		exit(1);
-	}
+    if (connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("tcpclient: connect");
+        close(s);
+        exit(1);
+    }
 
-	/* 
-		Constructing the registration packet at client.
-		This is the first packet sent and includes the code '121'
-		which indicates it is a registration packet.		
-	*/
-	packet_reg.type = htons(121);
-	strcpy(packet_reg.uName, userName);
-	strcpy(packet_reg.mName, computerName);
+    /*
+        Constructing the registration packet at client.
+        This is the first packet sent and includes the code '121'
+        which indicates it is a registration packet.
+    */
+    packet_reg.type = htons(121);
+    strcpy(packet_reg.uName, userName);
+    strcpy(packet_reg.mName, computerName);
 
-	/*
-		Send the registration packet to the server.
-		If it fails to send, the program exits.
-	*/
-	if(send(s, &packet_reg,sizeof(packet_reg),0) < 0)	
-	{
-		printf("\n Send failed\n");
-		exit(1);
-	}
-	/*
-		Get registration response.
-		After the server receives our registration it lets us know
-		by sending back a confirmation message with code '221'.
-	*/
-	if(recv(s, &packet_reg_confirm,sizeof(packet_reg_confirm),0) < 0)
-	{
-		printf("\n Did not receive registration confirmation packet \n");
-		exit(1);
-	}
-	/*
-		if the registration confirmation packet is the correct code
-		then we continue into the chat and can send chat messages.
-	*/
-	else if(ntohs(packet_reg_confirm.type) == 221)
-	{
-		printf("\n Registration Confirmed! \n");
-		/*main loop: get and send lines of text */
-		while(fgets(buf, sizeof(buf), stdin)){
-			buf[MAX_LINE-1] = '\0';
-			/*
-				Constructing the chat packet.
-				This is the packet that will contain the chat message
-				It uses the code '131' indicating it is a chat message.
-			*/
-			packet_chat.type = htons(131);
-			strcpy(packet_chat.uName, userName);
-			strcpy(packet_chat.mName, computerName);
-			strcpy(packet_chat.data, buf);
-			/*
-				Send the chat packet to the server 
-			*/
-			if(send(s, &packet_chat,sizeof(packet_chat),0) < 0)
-			{
-				printf("\n Send Failed \n");
-				exit(1);
-			}
-			/*
-				After the server receives the chat packet
-				it sends a chat response with code 231.
-				If the response code is not 231, we exit the program.
-			*/
-			if(recv(s, &packet_chat_response,sizeof(packet_chat_response),0) < 0)
-			{
-				printf("\n Did not receive chat response \n");
-				exit(1);
-			}
-			else if (ntohs(packet_chat_response.type) != 231){
-				printf("\n Bad chat response code %d \n", ntohs(packet_chat_response.type));
-				exit(1);
-			}
-			printf("\n Chat response for message %s \n", packet_chat_response.data);
-		}
-	}
-	/*
-		if the registration confirmation code is not 221
-		we exit the program
-	*/
-	else{
-		printf("\n Could not confirm registration \n");
-		exit(1);
-	}
+    /*
+        Send the registration packet to the server.
+        If it fails to send, the program exits.
+    */
+    if (send(s, &packet_reg, sizeof(packet_reg), 0) < 0) {
+        printf("\n Send failed\n");
+        exit(1);
+    } else {
+        printf("\n Sent registation request. Code: %d\n", ntohs(packet_reg.type));
+    }
+    /*
+        Get registration response.
+        After the server receives our registration it lets us know
+        by sending back a confirmation message with code '221'.
+    */
+    if (recv(s, &packet_reg_confirm, sizeof(packet_reg_confirm), 0) < 0) {
+        printf("\n Did not receive registration confirmation packet \n");
+        exit(1);
+    }
+        /*
+            if the registration confirmation packet is the correct code
+            then we continue into the chat and can send chat messages.
+        */
+
+    else if (ntohs(packet_reg_confirm.type) == 221) {
+        printf("\n Registration Confirmed! \n");
+        /*main loop: get and send lines of text */
+        printf("\n Send server a message: ");
+
+        while (fgets(buf, sizeof(buf), stdin)) {
+            buf[MAX_LINE - 1] = '\0';
+            /*
+                Constructing the chat packet.
+                This is the packet that will contain the chat message
+                It uses the code '131' indicating it is a chat message.
+            */
+            packet_chat.type = htons(131);
+            strcpy(packet_chat.uName, userName);
+            strcpy(packet_chat.mName, computerName);
+            strcpy(packet_chat.data, buf);
+
+            /*
+                Send the chat packet to the server
+            */
+            if (send(s, &packet_chat, sizeof(packet_chat), 0) < 0) {
+                printf("\n Send Failed \n");
+                exit(1);
+            }
+            /*
+                After the server receives the chat packet
+                it sends a chat response with code 231.
+                If the response code is not 231, we exit the program.
+            */
+            if (recv(s, &packet_chat_response, sizeof(packet_chat_response), 0) < 0) {
+                printf("\n Did not receive chat response \n");
+                exit(1);
+            } else if (ntohs(packet_chat_response.type) != 231) {
+                printf("\n Bad chat response code %d \n", ntohs(packet_chat_response.type));
+                exit(1);
+            }
+            printf("\n Chat response for message %s \n", packet_chat_response.data);
+            printf("\n Send server a message: ");
+        }
+    }
+        /*
+            if the registration confirmation code is not 221
+            we exit the program
+        */
+    else {
+        printf("\n Could not confirm registration \n");
+        exit(1);
+    }
 
 }
--- 1/tcpEchoServer.c
+++ 1/tcpEchoServer.c
@@ -5,7 +5,7 @@
 #include<netdb.h>
 #include<string.h>
 #include<stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 // #define SERVER_PORT 7777
 #define MAX_LINE 256
@@ -13,182 +13,172 @@
 #define MAXNAME 256
 
 /* structure of the packet */
-struct packet{
-	short type;
-	char uName[MAXNAME];
-	char mName[MAXNAME];
-	char data[MAXNAME];
+struct packet {
+    short type;
+    char uName[MAXNAME];
+    char mName[MAXNAME];
+    char data[MAXNAME];
 };
 
 /* structure of Registration Table */
-struct registrationTable{
-int port;
-int sockid;
-char mName[MAXNAME];
-char uName[MAXNAME];
+struct registrationTable {
+    int port;
+    int sockid;
+    char mName[MAXNAME];
+    char uName[MAXNAME];
 };
 
-int main(int argc, char* argv[])
-{
-	struct sockaddr_in sin;
-	struct sockaddr_in clientAddr;
-	char buf[MAX_LINE];
-	int s, new_s;
-	int len, index=0;
-	struct packet packet_reg;
-	struct packet packet_reg_confirm;
-	struct packet packet_chat;
-	struct packet packet_chat_response;
-	struct registrationTable table[10];
-	short SERVER_PORT;
+int main(int argc, char *argv[]) {
+    struct sockaddr_in sin;
+    struct sockaddr_in clientAddr;
+    char buf[MAX_LINE];
+    int s, new_s;
+    int len, index = 0;
+    struct packet packet_reg;
+    struct packet packet_reg_confirm;
+    struct packet packet_chat;
+    struct packet packet_chat_response;
+    struct registrationTable table[10];
+    short SERVER_PORT;
 
-	/*
-		The following grabs the command line arguments.
-		It will take a port number as a parameter otherwise
-		the port number will default to 7777.
-	*/
-	if(argc == 2){
-		SERVER_PORT = atoi(argv[1]);
-	}
-	else{
-		SERVER_PORT = 7777;
-	}	
+    /*
+        The following grabs the command line arguments.
+        It will take a port number as a parameter otherwise
+        the port number will default to 7777.
+    */
+    if (argc == 2) {
+        SERVER_PORT = atoi(argv[1]);
+    } else {
+        SERVER_PORT = 7777;
+    }
 
-	/* setup passive open */
-	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-		perror("tcpserver: socket");
-		exit(1);
-	}
+    /* setup passive open */
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("tcpserver: socket");
+        exit(1);
+    }
 
-	/* build address data structure */
-	bzero((char*)&sin, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_port = htons(SERVER_PORT);
+    /* build address data structure */
+    bzero((char *) &sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(SERVER_PORT);
 
-	if(bind(s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
-		perror("tcpclient: bind");
-		exit(1);
-	}
-	listen(s, MAX_PENDING);
+    if (bind(s, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("tcpclient: bind");
+        exit(1);
+    }
+    listen(s, MAX_PENDING);
 
-	len = sizeof(struct sockaddr_in);
+    len = sizeof(struct sockaddr_in);
 
-	/* wait for connection, then receive and print text */
-	while(1){
-		if((new_s = accept(s, (struct sockaddr *)&clientAddr, &len)) < 0){
-			perror("tcpserver: accept");
-			exit(1);
-		}
+    /* wait for connection, then receive and print text */
+    while (1) {
+        if ((new_s = accept(s, (struct sockaddr *) &clientAddr, &len)) < 0) {
+            perror("tcpserver: accept");
+            exit(1);
+        }
 
-		printf("\n Client's port is %d \n", ntohs(clientAddr.sin_port)); 
+        printf("\n Client's port is %d \n", ntohs(clientAddr.sin_port));
 
-		/*
-			Get registration packet
-			After establishing a connection, the client must register with
-			the server.
-		*/
-		if(recv(new_s, &packet_reg,sizeof(packet_reg),0) < 0)
-		{
-			printf("\n Could not receive first registration packet \n");
-			exit(1);
-		}
-		/*
-			if valid registration packet
-			This checks to make sure the registration packet code is 121.
-			If it is, register the client in the registration table and
-			send the registration confirmation to the client
-		*/
-		else if(ntohs(packet_reg.type) == 121)
-		{
-			printf("\n Client's info is %d", ntohs(packet_reg.type));
-			printf("\n %s", packet_reg.uName); 
-			printf("\n %s\n", packet_reg.mName);
+        /*
+            Get registration packet
+            After establishing a connection, the client must register with
+            the server.
+        */
+        if (recv(new_s, &packet_reg, sizeof(packet_reg), 0) < 0) {
+            printf("\n Could not receive first registration packet \n");
+            exit(1);
+        } else {
+            printf("\n Registration packet received: %d\n", ntohs(packet_reg.type));
+        }
+        /*
+            if valid registration packet
+            This checks to make sure the registration packet code is 121.
+            If it is, register the client in the registration table and
+            send the registration confirmation to the client
+        */
+        if (ntohs(packet_reg.type) == 121) {
+            printf("\n Client sent valid registration request. \n");
+            printf("\n %s", packet_reg.uName);
+            printf("\n %s\n", packet_reg.mName);
 
-			/* register client in the registration table */
-			table[index].port = clientAddr.sin_port;
-			table[index].sockid = new_s;
-			strcpy(table[index].uName, packet_reg.uName);
-			strcpy(table[index].mName, packet_reg.mName);
-			index++;
+            /* register client in the registration table */
+            table[index].port = clientAddr.sin_port;
+            table[index].sockid = new_s;
+            strcpy(table[index].uName, packet_reg.uName);
+            strcpy(table[index].mName, packet_reg.mName);
+            index++;
 
-			/*
-				build and send confirmation packet
-				the confirmation packet code is 221.
-				this code is returned to the client along with the
-				information the originally sent.
-			*/
-			packet_reg_confirm.type = htons(221);
-			strcpy(packet_reg_confirm.uName, packet_reg.uName);
-			strcpy(packet_reg_confirm.mName, packet_reg.mName);
-			if(send(new_s, &packet_reg_confirm,sizeof(packet_reg_confirm),0) < 0)	
-			{
-				printf("\n Send failed\n");
-				exit(1);
-			}
-			else
-			{
-				printf("\n Sent Confirmation Packet\n");
-			}
-		}
-		/*
-			not valid registration packet
-			if the registration packet code is not 121, send a packet back
-			to the client with a code other than 221, which indicates to the
-			client that there was a problem with their registration.
-		*/
-		else
-		{
-			packet_reg_confirm.type = htons(1);
-			if(send(new_s, &packet_reg_confirm,sizeof(packet_reg_confirm),0) < 0)	
-			{
-				printf("\n Send failed\n");
-				exit(1);
-			}
-			printf("\n %d is not a recognized command \n", ntohs(packet_reg.type)); 
-		}
+            /*
+                build and send confirmation packet
+                the confirmation packet code is 221.
+                this code is returned to the client along with the
+                information the originally sent.
+            */
+            packet_reg_confirm.type = htons(221);
+            strcpy(packet_reg_confirm.uName, packet_reg.uName);
+            strcpy(packet_reg_confirm.mName, packet_reg.mName);
+            if (send(new_s, &packet_reg_confirm, sizeof(packet_reg_confirm), 0) < 0) {
+                printf("\n Send failed\n");
+                exit(1);
+            } else {
+                printf("\n Sent Confirmation Packet\n");
+            }
+        }
+            /*
+                not valid registration packet
+                if the registration packet code is not 121, send a packet back
+                to the client with a code other than 221, which indicates to the
+                client that there was a problem with their registration.
+            */
+        else {
+            packet_reg_confirm.type = htons(1);
+            if (send(new_s, &packet_reg_confirm, sizeof(packet_reg_confirm), 0) < 0) {
+                printf("\n Send failed\n");
+                exit(1);
+            }
+            printf("\n %d is not a recognized command \n", ntohs(packet_reg.type));
+        }
 
-		/*
-			loop and continue to receive chat packets from the client.
-			valid chat packets have code 131
-		*/
-		while(len = recv(new_s, &packet_chat,sizeof(packet_chat),0))
-		{
-			/*
-				Check the chat packet type. If it is not 131,
-				Send a response type of 1, indicating an error.
-			*/
-			if(ntohs(packet_chat.type) != 131)
-			{
-				packet_chat_response.type = htons(1);
-				printf("\n Chat Packet type not recognized");
-			}
-			/*
-				If the chat packet type is 131, continue to print the chat
-				message and respond with the successful code 231
-			*/
-			else{
-				printf("%s: %s\n", table[index-1].uName, packet_chat.data);
-				/*
-					Build the chat response packet to the client.
-					chat reponse packets contain code 231 along with all of the
-					information contained in the client's chat packet.
-				*/
-				packet_chat_response.type = htons(231);
-				strcpy(packet_chat_response.uName, packet_chat.uName);
-				strcpy(packet_chat_response.mName, packet_chat.mName);
-				strcpy(packet_chat_response.data, packet_chat.data);
-			}
-			/*
-				Send the chat response packet back to the client.
-			*/
-			if(send(new_s, &packet_chat_response,sizeof(packet_chat_response),0) < 0)
-			{
-				printf("\n Send Failed \n");
-				exit(1);
-			}
-			printf("sending response %d\n", ntohs(packet_chat_response.type));
-		}
-		close(new_s);
-	}
+        /*
+            loop and continue to receive chat packets from the client.
+            valid chat packets have code 131
+        */
+        while (len = recv(new_s, &packet_chat, sizeof(packet_chat), 0)) {
+            /*
+                Check the chat packet type. If it is not 131,
+                Send a response type of 1, indicating an error.
+            */
+            if (ntohs(packet_chat.type) != 131) {
+                packet_chat_response.type = htons(1);
+                printf("\n Chat Packet type not recognized");
+            }
+                /*
+                    If the chat packet type is 131, continue to print the chat
+                    message and respond with the successful code 231
+                */
+            else {
+                printf("%s: %s\n", table[index - 1].uName, packet_chat.data);
+                /*
+                    Build the chat response packet to the client.
+                    chat reponse packets contain code 231 along with all of the
+                    information contained in the client's chat packet.
+                */
+                packet_chat_response.type = htons(231);
+                strcpy(packet_chat_response.uName, packet_chat.uName);
+                strcpy(packet_chat_response.mName, packet_chat.mName);
+                strcpy(packet_chat_response.data, packet_chat.data);
+            }
+            /*
+                Send the chat response packet back to the client.
+            */
+            if (send(new_s, &packet_chat_response, sizeof(packet_chat_response), 0) < 0) {
+                printf("\n Send Failed \n");
+                exit(1);
+            }
+            printf("sending response %d\n", ntohs(packet_chat_response.type));
+        }
+        close(new_s);
+    }
 }
