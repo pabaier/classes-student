6706932ed73484a69f4a550d285deb5f7325153e
pabaier <baierpa@g.cofc.edu>
2019-10-20 16:57:29 -0400

    parse command line arguments

--- project/sockets/client.py
+++ project/sockets/client.py
@@ -1,13 +1,19 @@
 # socket_echo_client.py
+import argparse
+import select
 import socket
 import sys
-import select 
+
+parser = argparse.ArgumentParser(description='Client Program.')
+parser.add_argument('--ip', "-i", type=str, default='localhost', dest="server_ip", help='server ip')
+parser.add_argument('--port', "-p", type=int, default=10000, dest="server_port", help='server port')
+args = parser.parse_args()
 
 # Create a TCP/IP socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = (args.server_ip, args.server_port)
 print('connecting to {} port {}'.format(*server_address))
 server.connect(server_address)
 
@@ -15,12 +21,12 @@ running = True
 while running:
 
     # maintains a list of possible input streams 
-    sockets_list = [sys.stdin, server] 
-  
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-  
-    for socks in read_sockets: 
-        if socks == server: 
+    sockets_list = [sys.stdin, server]
+
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
+    for socks in read_sockets:
+        if socks == server:
             message = socks.recv(2048).decode('utf-8')
             print(f'-{message}')
         else:
@@ -28,7 +34,7 @@ while running:
             message = sys.stdin.readline().rstrip()
             if message == 'exit':
                 running = False
-            server.send(str.encode(message)) 
+            server.send(str.encode(message))
             # sys.stdout.write("<You>") 
             # sys.stdout.write(message) 
             # sys.stdout.flush()
--- project/sockets/server.py
+++ project/sockets/server.py
@@ -1,33 +1,43 @@
-import socket, threading
+import argparse
+import socket
+import threading
+
+
 class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
+    def __init__(self, clientAddress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
         self.port = clientAddress[1]
-        print ("New connection added: ", clientAddress)
+        print("New connection added: ", clientAddress)
+
     def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
+        print("Connection from : ", clientAddress)
+        # self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         msg = ''
         while True:
             data = self.csocket.recv(2048)
             msg = data.decode()
-            if msg=='exit':
-              break
-            print (f'{self.port}: {msg}')
+            if msg == 'exit':
+                break
+            print(f'{self.port}: {msg}')
             for port in registration:
                 print(f'port {port} - myport {self.port}')
                 if not port == self.port:
                     print(f'sending to port {port}')
                     registration[port].send(str.encode(msg))
-        print (f'{self.port} exited')
+        print(f'{self.port} exited')
+
+
+parser = argparse.ArgumentParser(description='Server Program.')
+parser.add_argument('--ip', "-i", type=str, default='localhost', dest="server_ip", help='server ip')
+parser.add_argument('--port', "-p", type=int, default=10000, dest="server_port", help='server port')
+args = parser.parse_args()
 
 registration = {}
-LOCALHOST = "127.0.0.1"
-PORT = 10000
+server_address = (args.server_ip, args.server_port)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
+server.bind(server_address)
 print("Server started")
 print("Waiting for client request..")
 while True:
