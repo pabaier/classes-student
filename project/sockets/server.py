7c39fc7aeb1a6b2e40be134a937b6d6afdb0b4fe
pabaier <baierpa@g.cofc.edu>
2019-10-23 22:07:53 -0400

    poll clients every two seconds and report results

--- project/sockets/server.py
+++ project/sockets/server.py
@@ -4,6 +4,7 @@ import select
 import socket
 import sys
 import threading
+import time
 
 
 class ClientThread(threading.Thread):
@@ -22,10 +23,17 @@ class ClientThread(threading.Thread):
             if msg == 'exit':
                 registration.pop(self.port)
                 break
-            print(f'{self.port}: {msg}')
-            SensorData.append(float(msg))
+            SensorData[self.port] = (float(msg))
+        SensorData.pop(self.port)
         print(f'{self.port} exited')
 
+def printdata():
+    sensorSum = 0
+    if (len(SensorData) > 0):
+        for key in SensorData:
+            print(f'\t{key}: {SensorData[key]}')
+            sensorSum += SensorData[key]
+        print(f'   avg: {sensorSum / len(SensorData)}')
 
 parser = argparse.ArgumentParser(description='Server Program.')
 parser.add_argument('--ip', "-i", type=str, default='localhost', dest="server_ip", help='server ip')
@@ -33,7 +41,7 @@ parser.add_argument('--port', "-p", type=int, default=10000, dest="server_port",
 args = parser.parse_args()
 
 registration = {}
-SensorData = []
+SensorData = {}
 server_address = (args.server_ip, args.server_port)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -44,7 +52,6 @@ while running:
     server.listen(1)
     # maintains a list of possible input streams
     sockets_list = [sys.stdin, server]
-    print("---")
     read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
 
     for socks in read_sockets:
@@ -55,12 +62,11 @@ while running:
             newthread = ClientThread(clientAddress, clientsock)
             newthread.start()
         else:
-            message = sys.stdin.readline().rstrip()
-            if message == 'exit':
-                running = False
-                break
+            time.sleep(2)
+            print("---")
+            print("poll:")
+            printdata()
+            print("---")
             for client in registration:
-                registration[client].send(str.encode(message))
-            sys.stdout.flush()
-            print()
+                registration[client].send(str.encode("poll"))
 server.close()
