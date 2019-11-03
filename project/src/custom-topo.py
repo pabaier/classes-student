0d30aeb39b503421161acd08273e338ce1607c5a
pabaier <baierpa@g.cofc.edu>
2019-11-03 18:47:46 -0500

    read command line arguments

--- project/src/custom-topo.py
+++ project/src/custom-topo.py
@@ -10,17 +10,36 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.topolib import TreeTopo
 from mininet.log import setLogLevel
 from mininet.cli import CLI
+import argparse
 # from topologies import BusTopo
 import topologies
 
-setLogLevel( 'info' )
 
-c0 = Controller( 'c0' )
+def simpleTest(args):
+	c0 = Controller( 'c0' )
 
-topologies.num_switches = 7
-# bus = BusTopo()
-ring = topologies.Ring()
-net = Mininet(topo=ring, controller=c0)
-net.start()
-CLI( net )
-net.stop()
+	topologies.num_switches = args.switches
+	# bus = BusTopo()
+	# ring = topologies.Ring()
+	topo = getTopo(args.topo)
+	net = Mininet(topo=topo, controller=c0)
+	net.start()
+	CLI( net )
+	net.stop()
+
+def getTopo(topo):
+	switch = {
+		'ring': topologies.Ring(),
+		'bus': topologies.BusTopo()
+	}
+	return switch[topo]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Server Program.')
+    parser.add_argument('--switches', "-s", type=int, default=1, dest="switches", help='number of switches in network')
+    parser.add_argument('--topo', "-t", type=str, default='bus', dest="topo", help='which topology to use')
+    args = parser.parse_args()
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest(args)
