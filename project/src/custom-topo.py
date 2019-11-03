03d446ddeb40c37b6cdd5abe9de1d16854e9033c
pabaier <baierpa@g.cofc.edu>
2019-11-03 17:45:06 -0500

    add ring topology

--- project/src/custom-topo.py
+++ project/src/custom-topo.py
@@ -10,7 +10,7 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.topolib import TreeTopo
 from mininet.log import setLogLevel
 from mininet.cli import CLI
-from topologies import BusTopo
+# from topologies import BusTopo
 import topologies
 
 setLogLevel( 'info' )
@@ -18,8 +18,9 @@ setLogLevel( 'info' )
 c0 = Controller( 'c0' )
 
 topologies.num_switches = 7
-bus = BusTopo()
-net = Mininet(topo=bus, controller=c0)
+# bus = BusTopo()
+ring = topologies.Ring()
+net = Mininet(topo=ring, controller=c0)
 net.start()
 CLI( net )
 net.stop()
--- project/src/topologies.py
+++ project/src/topologies.py
@@ -15,3 +15,18 @@ class BusTopo( Topo ):
             if i > 0:
                 prevswitch = 's' + str(i)
                 self.addLink(prevswitch, switch)
+
+class Ring( Topo ):
+
+    def build( self ):
+        # Add hosts and switches
+        for i in range(num_switches):
+            host = 'h' + str(i+1)
+            switch = 's' + str(i+1)
+            self.addHost(host)
+            self.addSwitch(switch)
+            self.addLink(host, switch)
+            if i > 0:
+                prevswitch = 's' + str(i)
+                self.addLink(prevswitch, switch)
+        self.addLink('s1', 's' + str(num_switches))
