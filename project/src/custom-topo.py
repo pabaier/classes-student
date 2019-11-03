152e26c0897f76d1d170738a473f9ccd1cd7d2c5
pabaier <baierpa@g.cofc.edu>
2019-11-03 17:34:30 -0500

    work out how to set custom switch amount

--- project/src/custom-topo.py
+++ project/src/custom-topo.py
@@ -10,22 +10,15 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.topolib import TreeTopo
 from mininet.log import setLogLevel
 from mininet.cli import CLI
-# from topologies import topos
 from topologies import BusTopo
 import topologies
 
 setLogLevel( 'info' )
 
-# Two local and one "external" controller (which is actually c0)
-# Ignore the warning message that the remote isn't (yet) running
 c0 = Controller( 'c0' )
 
-# bus = topos['bustopo'](7)
 topologies.num_switches = 7
 bus = BusTopo()
-# BusTopo.num_switches=7
-bus.setSwitches(7)
-# BusTopo.setSwitches(8)
 net = Mininet(topo=bus, controller=c0)
 net.start()
 CLI( net )
--- project/src/topologies.py
+++ project/src/topologies.py
@@ -1,21 +1,8 @@
-"""Custom topology example
-Two directly connected switches plus a host for each switch:
-   host --- switch --- switch --- host
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
 from mininet.topo import Topo
 
 num_switches = 5
 
 class BusTopo( Topo ):
-    "Simple topology example."
-    # def __init__(self):
-    #     super(BusTopo, self).__init__()
-    #     self.num_switches = 1
-    def setSwitches(self, num):
-        num_switches = num
 
     def build( self ):
         # Add hosts and switches
@@ -28,5 +15,3 @@ class BusTopo( Topo ):
             if i > 0:
                 prevswitch = 's' + str(i)
                 self.addLink(prevswitch, switch)
-
-# topos = { 'bustopo': ( lambda: BusTopo() ) }
