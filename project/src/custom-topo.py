a91c33d6dcb5bba483d33834d71ed3abf730a772
pabaier <baierpa@g.cofc.edu>
2019-11-03 19:21:09 -0500

    support different switches

--- project/src/custom-topo.py
+++ project/src/custom-topo.py
@@ -6,7 +6,7 @@ different controllers, by creating a custom Switch() subclass.
 """
 
 from mininet.net import Mininet
-from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.node import OVSSwitch, IVSSwitch, UserSwitch, Controller, RemoteController
 from mininet.topolib import TreeTopo
 from mininet.log import setLogLevel
 from mininet.cli import CLI
@@ -16,14 +16,16 @@ import topologies
 
 
 def simpleTest(args):
-	c0 = Controller( 'c0' )
+    c0 = Controller( 'c0' )
 
-	topologies.num_switches = args.switches
-	topo = getTopo(args.topo)
-	net = Mininet(topo=topo, controller=c0)
-	net.start()
-	CLI( net )
-	net.stop()
+    topologies.num_switches = args.switch_count
+    topo = getTopo(args.topo)
+    switch = getSwitch(args.switch)
+
+    net = Mininet(topo=topo, controller=c0, switch=switch)
+    net.start()
+    CLI( net )
+    net.stop()
 
 def getTopo(topo):
 	switch = {
@@ -32,11 +34,21 @@ def getTopo(topo):
 	}
 	return switch[topo]
 
+def getSwitch(switch):
+    s = {
+        'ovs': OVSSwitch,
+        'ivs': IVSSwitch,
+        'user': UserSwitch
+    }
+    return s[switch]
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Server Program.')
-    parser.add_argument('--switches', "-s", type=int, default=1, dest="switches", help='number of switches in network')
+    parser.add_argument('--switch_count', "-sc", type=int, default=1, dest="switch_count", help='number of switches in network')
     parser.add_argument('--topo', "-t", type=str, default='bus', dest="topo", help='which topology to use')
+    parser.add_argument('--switch', "-s", type=str, default='ovs', dest="switch", help='which switch to use')
+
     args = parser.parse_args()
     # Tell mininet to print useful information
     setLogLevel('info')
