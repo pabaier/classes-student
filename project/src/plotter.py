7fac7d40b87e4a80ff1fe7e42ef0c47e56cd5d3f
pabaier <baierpa@g.cofc.edu>
2019-11-07 21:24:07 -0500

    plot output file

---
+++ project/src/plotter.py
@@ -0,0 +1,30 @@
+# Average host ping rtt for 1st, 2nd, 3rd and 4th times
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+def run(filename):
+	runs = []
+	with open(filename) as file:
+		topology = file.readline().strip()
+		nodes = file.readline().strip()
+		run = file.readline().strip()
+		while run:
+			runs.append(float(run))
+			run = file.readline().strip()
+	x_axis = list(range(1,len(runs)+1))
+
+	plt.plot(x_axis, runs, linestyle='--', marker='o')
+	plt.title(f'Average Pingall Round Trip Times for {topology} with {nodes} nodes')
+	plt.ylabel('Ping RTT (ms)')
+	plt.xlabel('Runs')
+
+	plt.savefig(f'{topology}-{nodes}.png')
+	# plt.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Pretty Picture Program.')
+    parser.add_argument('--filename', "-f", type=str, dest="filename", help='name of the data file')
+
+    args = parser.parse_args()
+    run(args.filename)
