d2286582f51b4c6073e22f421f5fe598f9d30452
pabaier <baierpa@g.cofc.edu>
2019-11-14 13:55:12 -0500

    use test plan with interrupts

--- project/src/programs.py
+++ project/src/programs.py
@@ -15,6 +15,7 @@ class Program():
 		self.controller = Controller( 'c0' )
 		self.topology = topologies.topos['bus']
 		self.switch = OVSSwitch
+		self.tests = []
 
 	def set_number_of_switches(self, num):
 		self.num_switches = num
@@ -42,6 +43,10 @@ class Program():
 		self.switch = s[switch]
 		return self
 	
+	def add_tests(self, test):
+		self.tests.append(test)
+		return self
+
 	def build(self):
 		self.net = Mininet(topo=self.topology, controller=self.controller, switch=self.switch)
 		return self
@@ -50,13 +55,13 @@ def a():
 	a = Program().set_controller('default').set_number_of_switches(5).set_topology('mesh').build()
 	net = a.net
 	net.start()
-
 	test = tests['pingallfull'](net, 'avgrtt')
-	test.run()
-	test.run()
-	net.configLinkStatus('s1', 's2', 'down')
-	test.run()
-	test.run()
+	links_down = tests['linkinterrupt'](net, ['s1', 's2'], 'down')
+	links_up = tests['linkinterrupt'](net, ['s1', 's2'], 'up')
+
+	testPlan = [test, test, links_down, test, test, links_up, test, test]
+	for test in testPlan:
+		test.run()
 	net.stop()
 
 a()
