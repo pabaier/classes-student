0cc54bcfae4fb5f770f6fc35f0c2dcce2fde1e16
pabaier <baierpa@g.cofc.edu>
2019-11-14 22:36:13 -0500

    add to main test

--- project/src/main.py
+++ project/src/main.py
@@ -5,11 +5,18 @@ import argparse
 a = Program().set_controller('default').set_number_of_switches(5).set_topology('mesh').build()
 net = a.net
 net.start()
-test = tests['pingallfull'](net)
-links_down = tests['linkinterrupt'](net, ['s1', 's2'], 'down')
-links_up = tests['linkinterrupt'](net, ['s1', 's2'], 'up')
+pingAll = tests['pingall'](net)
+pingfull = tests['pingallfull'](net)
+s1_s2_down = tests['linkinterrupt'](net, ['s1', 's2'], 'down')
+s1_s2_up = tests['linkinterrupt'](net, ['s1', 's2'], 'up')
+s1_s3_down = tests['linkinterrupt'](net, ['s1', 's3'], 'down')
+s1_s3_up = tests['linkinterrupt'](net, ['s1', 's3'], 'up')
 
-testPlan = [test, test, links_down, test, test, links_up, test, test]
+testPlan = [pingAll, pingfull, s1_s2_down, s1_s3_down, pingAll, pingfull, s1_s2_up, s1_s3_up, pingAll, pingfull]
 for test in testPlan:
 	test.run()
+	if test.type == 'pingallfull':
+		avg = test.getStats('avgrtt')
+		print str(avg)
+
 net.stop()
