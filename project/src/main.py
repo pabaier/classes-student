98879b51bda8d416266736a53831ea570550598f
pabaier <baierpa@g.cofc.edu>
2019-11-14 23:40:15 -0500

    use list of switches for link interrupt in main

--- project/src/main.py
+++ project/src/main.py
@@ -7,16 +7,13 @@ net = a.net
 net.start()
 pingAll = tests['pingall'](net)
 pingfull = tests['pingallfull'](net)
-s1_s2_down = tests['linkinterrupt'](net, ['s1', 's2'], 'down')
-s1_s2_up = tests['linkinterrupt'](net, ['s1', 's2'], 'up')
-s1_s3_down = tests['linkinterrupt'](net, ['s1', 's3'], 'down')
-s1_s3_up = tests['linkinterrupt'](net, ['s1', 's3'], 'up')
-s1_s4_down = tests['linkinterrupt'](net, ['s1', 's4'], 'down')
-s1_s4_up = tests['linkinterrupt'](net, ['s1', 's4'], 'up')
-s1_s5_down = tests['linkinterrupt'](net, ['s1', 's5'], 'down')
-s1_s5_up = tests['linkinterrupt'](net, ['s1', 's5'], 'up')
+switches = []
+for i in range(2,6):
+	switches.append(('s1', 's' + str(i)))
+links_down = tests['linkinterrupt'](net, switches, 'down')
+links_up = tests['linkinterrupt'](net, switches, 'up')
 
-testPlan = [pingAll, pingfull, s1_s2_down, s1_s3_down, s1_s4_down, s1_s5_down, pingAll, pingfull, s1_s2_up, s1_s3_up, s1_s4_up, s1_s5_up,  pingAll, pingfull]
+testPlan = [pingAll, pingfull, links_down, pingAll, pingfull, links_up,  pingAll, pingfull]
 for test in testPlan:
 	test.run()
 	if test.type == 'pingallfull':
