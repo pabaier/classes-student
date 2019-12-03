eec84ce840a8fe86154c14bf3eb1c8aed53f27c8
pabaier <baierpa@g.cofc.edu>
2019-12-03 18:33:38 -0500

    generalize number of controllers and split out topology

--- project/src/highwinds.py
+++ project/src/highwinds.py
@@ -4,7 +4,7 @@ import argparse
 from mininet.cli import CLI
 from mininet.node import Controller,OVSSwitch
 from mininet.net import Mininet
-import topologies
+from topologies import topos
 
 def main():
 	down_pairs = [(1,None), (2,None), (3,None), (4,None), (5,None), 
@@ -29,23 +29,20 @@ def runTest(d):
 
 	for test in range(2):
 		net = Mininet( controller=Controller, switch=OVSSwitch )
-		c1, c2, c3, c4, c5 = buildTopology(net)
+		controllers = topos['highwinds']().build(net, 5)
 
 		# tests
 		pingfull = tests['pingallfull'](net)
-		controller1_toggle = tests['controllerinterrupt'](net, c1)
-		controller2_toggle = tests['controllerinterrupt'](net, c2)
-		controller3_toggle = tests['controllerinterrupt'](net, c3)
-		controller4_toggle = tests['controllerinterrupt'](net, c4)
-		controller5_toggle = tests['controllerinterrupt'](net, c5)
-		toggles = {1:controller1_toggle, 2:controller2_toggle, 3:controller3_toggle, 4:controller4_toggle, 5:controller5_toggle}
+		toggles = []
+		for c in controllers:
+			toggles.append(tests['controllerinterrup'](net, c))
 
 		if test == 0:
 			plan = [pingfull, pingfull]
 		elif d[1]:
-			plan = [toggles[d[0]], toggles[d[1]], pingfull, pingfull]
+			plan = [toggles[d[0]-1], toggles[d[1]-1], pingfull, pingfull]
 		else:
-			plan = [toggles[d[0]], pingfull, pingfull]
+			plan = [toggles[d[0]-1], pingfull, pingfull]
 		for test in plan:
 			test.run()
 			if test.type == 'pingallfull':
@@ -56,90 +53,6 @@ def runTest(d):
 		# CLI (net)
 		net.stop()
 
-def buildTopology(net):
-	# controllers
-	c1 = net.addController( 'c1', port=6633 )
-	c2 = net.addController( 'c2', port=6634 )
-	c3 = net.addController( 'c3', port=6635 )
-	c4 = net.addController( 'c4', port=6637 )
-	c5 = net.addController( 'c5', port=6638 )
-	controllers = (c1, c2, c3, c4, c5)
-
-	# add switches
-	for i in range(18):
-		switch = 's' + str(i+1)
-		net.addSwitch(switch, stp=True, failMode='standalone')
-	# addhosts
-	for i in range(18):
-		host = 'h' + str(i+1)
-		net.addHost(host)
-	# links
-	for i in range(18):
-		net.addLink(net.hosts[i], net.switches[i])
-	switch = net.switches
-	switches = [None]
-	for s in switch:
-		switches.append(s)
-	net.addLink(switches[1],switches[2])
-	net.addLink(switches[1],switches[11])
-	net.addLink(switches[2],switches[3])
-	net.addLink(switches[2],switches[8])
-	net.addLink(switches[3],switches[4])
-	net.addLink(switches[3],switches[5])
-	net.addLink(switches[3],switches[11])
-	net.addLink(switches[4],switches[5])
-	net.addLink(switches[4],switches[7])
-	net.addLink(switches[5],switches[6])
-	net.addLink(switches[5],switches[8])
-	net.addLink(switches[5],switches[11])
-	net.addLink(switches[6],switches[7])
-	net.addLink(switches[6],switches[12])
-	net.addLink(switches[7],switches[8])
-	net.addLink(switches[7],switches[11])
-	net.addLink(switches[8],switches[9])
-	net.addLink(switches[8],switches[11])
-	net.addLink(switches[8],switches[15])
-	net.addLink(switches[9],switches[10])
-	net.addLink(switches[9],switches[11])
-	net.addLink(switches[9],switches[16])
-	net.addLink(switches[10],switches[11])
-	net.addLink(switches[12],switches[13])
-	net.addLink(switches[14],switches[15])
-	net.addLink(switches[14],switches[17])
-	net.addLink(switches[15],switches[16])
-	net.addLink(switches[15],switches[18])
-	net.addLink(switches[16],switches[17])
-	net.addLink(switches[17],switches[18])
-
-	net.build()
-	c1.start()
-	c2.start()
-	c3.start()
-	c4.start()
-	c5.start()
-	c1Switches = ['s1', 's2', 's3', 's4']
-	c2Switches = ['s5', 's6', 's7']
-	c3Switches = ['s8', 's9', 's10', 's11']
-	c4Switches = ['s12', 's13']
-	c5Switches = ['s14', 's15', 's16', 's17', 's18']
-	
-	for s in c1Switches:
-		switch = net.getNodeByName(s)
-		switch.start([c1])
-	for s in c2Switches:
-		switch = net.getNodeByName(s)
-		switch.start([c2])
-	for s in c3Switches:
-		switch = net.getNodeByName(s)
-		switch.start([c3])
-	for s in c4Switches:
-		switch = net.getNodeByName(s)
-		switch.start([c4])
-	for s in c5Switches:
-		switch = net.getNodeByName(s)
-		switch.start([c5])
-
-	return controllers
 	
 if __name__ == '__main__':
 	main()
--- project/src/topologies.py
+++ project/src/topologies.py
@@ -64,47 +64,78 @@ class Mesh( Topo ):
 
 class Highwinds( Topo ):
 
-    def build( self ):
-        # Add hosts and switches
+    def build( self, net, controller_amount ):
+        port = 6633
+        controllers = []
+        for i in range(controller_amount):
+            controllers.append(net.addController( 'c' + str(i + 1), port=port))
+            port += 1
+
+        # add switches
+        for i in range(18):
+            switch = 's' + str(i+1)
+            net.addSwitch(switch, stp=True, failMode='standalone')
+        # addhosts
         for i in range(18):
             host = 'h' + str(i+1)
-            switch = 's' + str(i+1)
-            self.addHost(host)
-            self.addSwitch(switch, stp=True, failMode='standalone')
-            self.addLink(host, switch)
-        switch = self.switches()
+            net.addHost(host)
+        # links
+        for i in range(18):
+            net.addLink(net.hosts[i], net.switches[i])
+        switch = net.switches
         switches = [None]
         for s in switch:
             switches.append(s)
-        self.addLink(switches[1],switches[2])
-        self.addLink(switches[1],switches[11])
-        self.addLink(switches[2],switches[3])
-        self.addLink(switches[2],switches[8])
-        self.addLink(switches[3],switches[4])
-        self.addLink(switches[3],switches[5])
-        self.addLink(switches[3],switches[11])
-        self.addLink(switches[4],switches[5])
-        self.addLink(switches[4],switches[7])
-        self.addLink(switches[5],switches[6])
-        self.addLink(switches[5],switches[8])
-        self.addLink(switches[5],switches[11])
-        self.addLink(switches[6],switches[7])
-        self.addLink(switches[6],switches[12])
-        self.addLink(switches[7],switches[8])
-        self.addLink(switches[7],switches[11])
-        self.addLink(switches[8],switches[9])
-        self.addLink(switches[8],switches[11])
-        self.addLink(switches[8],switches[15])
-        self.addLink(switches[9],switches[10])
-        self.addLink(switches[9],switches[11])
-        self.addLink(switches[9],switches[16])
-        self.addLink(switches[10],switches[11])
-        self.addLink(switches[12],switches[13])
-        self.addLink(switches[14],switches[15])
-        self.addLink(switches[14],switches[17])
-        self.addLink(switches[15],switches[16])
-        self.addLink(switches[15],switches[18])
-        self.addLink(switches[16],switches[17])
-        self.addLink(switches[17],switches[18])
+        net.addLink(switches[1],switches[2])
+        net.addLink(switches[1],switches[11])
+        net.addLink(switches[2],switches[3])
+        net.addLink(switches[2],switches[8])
+        net.addLink(switches[3],switches[4])
+        net.addLink(switches[3],switches[5])
+        net.addLink(switches[3],switches[11])
+        net.addLink(switches[4],switches[5])
+        net.addLink(switches[4],switches[7])
+        net.addLink(switches[5],switches[6])
+        net.addLink(switches[5],switches[8])
+        net.addLink(switches[5],switches[11])
+        net.addLink(switches[6],switches[7])
+        net.addLink(switches[6],switches[12])
+        net.addLink(switches[7],switches[8])
+        net.addLink(switches[7],switches[11])
+        net.addLink(switches[8],switches[9])
+        net.addLink(switches[8],switches[11])
+        net.addLink(switches[8],switches[15])
+        net.addLink(switches[9],switches[10])
+        net.addLink(switches[9],switches[11])
+        net.addLink(switches[9],switches[16])
+        net.addLink(switches[10],switches[11])
+        net.addLink(switches[12],switches[13])
+        net.addLink(switches[14],switches[15])
+        net.addLink(switches[14],switches[17])
+        net.addLink(switches[15],switches[16])
+        net.addLink(switches[15],switches[18])
+        net.addLink(switches[16],switches[17])
+        net.addLink(switches[17],switches[18])
 
-topos = { 'bus': ( lambda: Bus() ), 'ring': ( lambda: Ring() ), 'star': ( lambda: Star() ), 'mesh': ( lambda: Mesh() ), 'highwinds': ( lambda: Highwinds() ) }
+        net.build()
+        for controller in controllers:
+            controller.start()
+
+        if controller_amount == 5:
+            switchMap = [['s1', 's2', 's3', 's4'],['s5', 's6', 's7'],['s8', 's9', 's10', 's11'], ['s12', 's13'],['s14', 's15', 's16', 's17', 's18'] ]
+        else:
+            switchMap = [['s1', 's2', 's3', 's4'],['s5', 's6', 's7'], ['s8', 's9', 's10', 's11'], ['s12', 's13'], ['s15', 's16'], ['s14', 's17', 's18']]
+
+        for i in range(len(switchMap)):
+            for s in switchMap[i]:
+                switch = net.getNodeByName(s)
+                switch.start(controllers[i])
+
+        return controllers
+
+topos = { 'bus': ( lambda: Bus() ), 
+'ring': ( lambda: Ring() ), 
+'star': ( lambda: Star() ), 
+'mesh': ( lambda: Mesh() ), 
+'highwinds': ( lambda: Highwinds() 
+) }
