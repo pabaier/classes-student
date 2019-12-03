a5ebd5dbd4507801497cd3669781b7c0cb577ffe
pabaier <baierpa@g.cofc.edu>
2019-12-03 13:30:32 -0500

    make title variable

--- project/src/internet2.py
+++ project/src/internet2.py
@@ -10,23 +10,22 @@ def main():
 	down_pairs = [(1,None), (2,None), (3,None), (4,None), (5,None), 
 				(1,2), (1,3), (1,4), (1,5),
 				(2,3), (2,4), (2,5),
-				(3,4), (3,5), (4,5) ]
+				(3,4), (3,5), (4,5)]
 	for d in down_pairs:
 		runTest(d)
 
 def runTest(d):
+	topologyName = "Internet2-OSE3-5"
 	if d[1]:
-		filename = "Internet2-OSE3-5-c" + str(d[0]) + "-c" + str(d[1])
+		filename = topologyName + "-c" + str(d[0]) + "-c" + str(d[1])
 		subtitle = "Average RTT with c" + str(d[0]) + " and c" + str(d[1])+ " Down"
 	else:
-		filename = "Internet2-OSE3-5-c" + str(d[0])
+		filename = topologyName + "-c" + str(d[0])
 		subtitle = "Average RTT with c" + str(d[0]) + " Down"
-	title = "Internet2-OSE3 5 Controllers"
+	title = topologyName + " Controllers"
 	f = open("data/" + filename, "a")
 	f.write("{}\n".format(title))
 	f.write("{}\n".format(subtitle))
-
-	title = "Internet2-OSE3 5 Controllers"
 
 	for test in range(2):
 		net = Mininet( controller=Controller, switch=OVSSwitch )
@@ -147,12 +146,6 @@ def runTest(d):
 
 		# CLI (net)
 		net.stop()
-
-def openfile(fileName, title, subtitle):
-	f = open("data/" + fileName, "a")
-	f.write("{}\n".format(title))
-	f.write("{}\n".format(subtitle))
-	return f
 
 if __name__ == '__main__':
 	main()
