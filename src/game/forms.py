69050c1c865f246c71e887df16684f6d203232bd
pabaier <baierpa@g.cofc.edu>
2019-12-30 11:51:52 -0500

    submit form with jquery

--- src/game/forms.py
+++ src/game/forms.py
@@ -1,5 +1,18 @@
 from django import forms
-from .models import Question
+from .models import Question, Game
+
+# class AddQuestionsForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Question
+#         fields = ['id', 'text']
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['members'] = forms.ModelMultipleChoiceField(
+#             queryset = Question.objects.all(),
+#             widget  = forms.CheckboxSelectMultiple(),
+#         )
 
 class AddQuestionsForm(forms.Form):
 	choices = forms.ModelMultipleChoiceField(
--- src/game/views.py
+++ src/game/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from game.models import Question
+from django.views.generic import ListView, View
+from game.models import Question, Game, Game_Question
 from .forms import AddQuestionsForm
+from django.http import HttpResponse
+from users.models import CustomUser
+import json
+import logging
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+logger = logging.getLogger(__name__)
 
 class QuestionList(ListView):
     model = Question
@@ -12,13 +20,42 @@ class QuestionList(ListView):
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
-        print(form)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            # <process form cleaned data>
-            return HttpResponseRedirect('/success/')
+        # request.POST = {'csrfmiddlewaretoken':#, 'name':'hi-ho', selected:"{'id':1, 'time':15},..."}
+        selected = json.loads(request.POST['selected'])
+        name = request.POST['gameName']
+        game = Game.objects.create(name=name, creator_user_id=request.user)
+        game.save()
+        logger.info(f'game saved: {game.name} with id {game.id}')
+        logger.info(f'adding {len(selected)} questions to game')
+        for selection in selected:
+            question = Question.objects.get(id=selection['id'])
+            game_question = Game_Question.objects.create(
+                game_id=game, 
+                question_id=question,
+                time_limit=selection['time']
+            )
 
-        return render(request, self.template_name, {'form': form})
+        return HttpResponse(json.dumps({'key': 'value'}))
+        # return render(request, self.template_name, {'form': form})
+
+class GameList(ListView):
+    template_name = 'MyGames.html'
+    # queryset = Game.objects.get(creator_user_id=request.user.id)
+    queryset = Game.objects.all()
+    context_object_name = 'games'
+
+class GameDetail(DetailView):
+    model = Game
+class GameCreate(CreateView):
+    template_name = 'game_create.html'
+    fields = ['name']
+    model = Game
+class GameUpdate(UpdateView):
+    template_name = 'game_update.html'
+    fields = ['name']
+    model = Game
+class GameDelete(DeleteView):
+    model = Game
