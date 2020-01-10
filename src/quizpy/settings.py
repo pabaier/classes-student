30202ab95aef73f27b84a4195488be5d26f6a4d1
pabaier <baierpa@g.cofc.edu>
2020-01-10 18:56:37 -0500

    authenticate with jwt

--- src/quizpy/settings.py
+++ src/quizpy/settings.py
@@ -154,6 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
--- src/quizpy/urls.py
+++ src/quizpy/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('games/', include('game.urls')),
     path('questions/', include('question.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/jwt-auth/', obtain_jwt_token),
 ]
