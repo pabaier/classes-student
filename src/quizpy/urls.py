9ede549585dea1cc5d4676f5c349b8762e6cd9f3
pabaier <baierpa@g.cofc.edu>
2020-01-10 13:53:11 -0500

    remove old front end files

--- src/quizpy/urls.py
+++ src/quizpy/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import home_view, signup_view
 
 urlpatterns = [
     path('games/', include('game.urls')),
     path('questions/', include('question.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    # path('', home_view, name="home"),
-    # path('signup/', signup_view, name="signup"),
 ]
