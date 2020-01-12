4b22dd42cacaf1de06781800c88d89acfdf4760a
pabaier <baierpa@g.cofc.edu>
2020-01-12 14:15:47 -0500

    include all game models in admin

--- src/game/admin.py
+++ src/game/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
 
-from .models import Game, Game_Question, Option
+from .models import Game, Game_Question, Active_Game, Option, Game_Option
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['text', 'a', 'b', 'c', 'd', 
-				'answer', 'public', 'creator']
+    fields = "__all__"
 
 class GameAdmin(admin.ModelAdmin):
-    fields = ['creator', 'name']
+    fields = "__all__"
 
 class GameQuestionAdmin(admin.ModelAdmin):
-    fields = ['game', 'question', 'time_limit']
+    fields = "__all__"
+
+class ActiveGame(admin.ModelAdmin):
+    fields = "__all__"
 
 class OptionAdmin(admin.ModelAdmin):
+    fields = "__all__"
+
+class GameOptionAdmin(admin.ModelAdmin):
     fields = "__all__"
 
 # admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Game, GameAdmin)
 # admin.site.register(Game_Question, GameQuestionAdmin)
 
-models = [Game, Game_Question, Option]
+models = [Game, Game_Question, Active_Game, Option, Game_Option]
 for model in models:
 	admin.site.register(model)
