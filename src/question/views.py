8c8fe20e3c7ffd8b59284b6b35675bb3a57bb4ec
pabaier <baierpa@g.cofc.edu>
2020-01-12 18:51:23 -0500

    generically CRUD all question objects

--- src/question/views.py
+++ src/question/views.py
@@ -1,7 +1,19 @@
-from .models import Question
-from .serializers import QuestionSerializer
+from .models import Question, Question_Game, Question_Answer_Option
+from .serializers import QuestionSerializer, QuestionGameSerializer, QuestionAnswerOptionSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 class QuestionViewSet(ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+class QuestionGameViewSet(ModelViewSet):
+    serializer_class = QuestionGameSerializer
+    queryset = Question_Game.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+class QuestionAnswerOptionViewSet(ModelViewSet):
+    serializer_class = QuestionAnswerOptionSerializer
+    queryset = Question_Answer_Option.objects.all()
+    permission_classes = (IsAuthenticated,)
