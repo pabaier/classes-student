25bb54eb46a5ad3738bb7e463630b75901a5ebf3
pabaier <baierpa@g.cofc.edu>
2020-01-11 20:23:05 -0500

    serialize all game app models

--- src/game/serializers.py
+++ src/game/serializers.py
@@ -1,4 +1,4 @@
-from .models import Game, Game_Question, Active_Game
+from .models import Game, Game_Question, Active_Game, Option, Game_Option
 from rest_framework import serializers
 
 class GameSerializer(serializers.ModelSerializer):
@@ -6,3 +6,23 @@ class GameSerializer(serializers.ModelSerializer):
 		model = Game
 		fields = "__all__"
 		read_only_fields = ("creator",)
+
+class GameQuestionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Game_Question
+		fields = "__all__"
+
+class ActiveGameSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Active_Game
+		fields = "__all__"
+
+class OptionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model: Option
+		fields = "__all__"
+
+class GameOptionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model: Game_Option
+		fields = "__all__"
