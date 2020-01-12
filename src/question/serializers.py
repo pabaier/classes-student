a85c6d4419e4037709cc5f5281a689b0e72d80bb
pabaier <baierpa@g.cofc.edu>
2020-01-12 18:47:59 -0500

    generically serialize all question app models

--- src/question/serializers.py
+++ src/question/serializers.py
@@ -1,7 +1,17 @@
-from .models import Question
+from .models import Question, Question_Game, Question_Answer_Option
 from rest_framework.serializers import ModelSerializer
 
 class QuestionSerializer(ModelSerializer):
     class Meta:
         model = Question
+        fields = "__all__"
+
+class QuestionGameSerializer(ModelSerializer):
+    class Meta:
+        model = Question_Game
+        fields = "__all__"
+
+class QuestionAnswerOptionSerializer(ModelSerializer):
+    class Meta:
+        model = Question_Answer_Option
         fields = "__all__"
