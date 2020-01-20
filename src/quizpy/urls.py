3b63d086c799ea6a02cec7a058b6cacf29882887
pabaier <baierpa@g.cofc.edu>
2020-01-19 21:39:53 -0500

    provide endpoint for user CRUD

--- src/quizpy/urls.py
+++ src/quizpy/urls.py
@@ -5,6 +5,7 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify
 urlpatterns = [
     path('games/', include('game.urls')),
     path('questions/', include('question.urls')),
+    path('users/', include('user.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/jwt-auth/', obtain_jwt_token),
