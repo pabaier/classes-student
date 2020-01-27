c908b8f5cb5443911502cd80d687a6efd163fcac
pabaier <baierpa@g.cofc.edu>
2020-01-27 12:12:05 -0500

    use thunk middleware for api calls

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
+import thunk from "redux-thunk"
 
 const store = createStore(
 		rootReducer,
+		applyMiddleware(thunk),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
 
