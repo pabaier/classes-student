14a69e79c1dfee2a5f380a9f7f790287493ed9b7
pabaier <baierpa@g.cofc.edu>
2020-01-30 21:21:32 -0500

    remove drf-jwt

--- backend/src/quizpy/settings.py
+++ backend/src/quizpy/settings.py
@@ -157,17 +157,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
-}
-
-JWT_AUTH = {
-    'JWT_ALLOW_REFRESH':True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
--- backend/src/quizpy/urls.py
+++ backend/src/quizpy/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('games/', include('game.urls')),
@@ -8,7 +7,7 @@ urlpatterns = [
     path('users/', include('user.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('auth/jwt-auth/', obtain_jwt_token),
-    path('auth/api-token-refresh/', refresh_jwt_token),
-    path('auth/api-token-verify/', verify_jwt_token),
+    # path('auth/jwt-auth/', obtain_jwt_token),
+    # path('auth/api-token-refresh/', refresh_jwt_token),
+    # path('auth/api-token-verify/', verify_jwt_token),
 ]
