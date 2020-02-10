a9610902142548eaea16b83a61d3c917731b5571
pabaier <baierpa@g.cofc.edu>
2020-02-09 22:23:37 -0500

    outline game start

--- backend/src/play/urls.py
+++ backend/src/play/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('host/<str:game_token>/', views.host, name='host'),
+    path('host/', views.host, name='host'),
     path('join/<str:game_token>/', views.join, name='join'),
 ]
--- backend/src/play/views.py
+++ backend/src/play/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from random import randint
 
 def index(request):
     return render(request, 'play/index.html', {})
 
-def host(request, game_token):
+def host(request):
     return render(request, 'play/host.html', {
-        'game_token': game_token
+        'game_token': randint(1,10)
     })
 
 def join(request, game_token):
