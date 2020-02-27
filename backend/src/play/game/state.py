d0324895f5382f916db7ef49975762afd62b9d98
pabaier <baierpa@g.cofc.edu>
2020-02-26 20:58:43 -0500

    camelCase state

--- backend/src/play/game/state.py
+++ backend/src/play/game/state.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 class State(str, Enum):
     REGISTRATION = 'registration'
-    POST_REGISTRATION = 'post_registration'
-    PRE_QUESTION = 'pre_question'
+    POST_REGISTRATION = 'postRegistration'
+    PRE_QUESTION = 'preQuestion'
     QUESTION = 'question'
     STANDBY = 'standby'
-    POST_QUESTION = 'post_question'
+    POST_QUESTION = 'postQuestion'
     FINISHED = 'finished'
-    GAME_OVER = 'game_over'
+    GAME_OVER = 'gameOver'
