30f1d847bb43dfaeae608a92f335d61fa5a401f1
pabaier <baierpa@g.cofc.edu>
2020-03-10 13:28:19 -0400

    display countdown timer

--- frontend/src/components/play/timer.js
+++ frontend/src/components/play/timer.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Timer = ({time, sendMessage}) => {
+	const [timeLeft, setTimeLeft] = useState(time);
+	const timeStart = useRef(Date.now());
+	
+	const calculateTimeLeft = () => {
+		var millis = Date.now() - timeStart.current;
+		var left = time - Math.floor(millis/1000)
+		setTimeLeft(left);
+		if(left <= 0){
+			stop();
+		}
+		return left;
+	}
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+		  setTimeLeft(calculateTimeLeft());
+		}, 1000);
+		return () => {clearTimeout(timer)};
+	  });
 
 	const stop = () => {
 		sendMessage('stop');
 	}
 
-	setTimeout(() => { stop(); }, time * 1000);
-
 	return (
-		<div>Timer: {time}</div>
+		<div>
+			<div>Timer: {timeLeft}</div>
+		</div>
 	)
 }
 
