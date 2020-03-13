343a16184c14b79eddccfa413e2e21c5b319f2d3
pabaier <baierpa@g.cofc.edu>
2020-03-12 21:15:53 -0400

    use only state variables in timer and comment timer

--- frontend/src/components/play/timer.js
+++ frontend/src/components/play/timer.js
@@ -1,22 +1,29 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Timer = ({time, sendMessage, startTime}) => {
+	// use one state variable for all variables so when any values change the timer only renders once
 	const [vars, setVars] = useState({'timeLeft': time, 'startTime': startTime})
-	const t = useRef(time);
 
 	const calculateTimeLeft = () => {
-		t.current -= 1;
-		if(t.current < 0)
+		// when the clock hits 0, tell the host to grab the next state
+		if(vars.timeLeft < 1)
 			stop()
+		// re-renders the timer with the new time but not after it stops
 		else
-			setVars({...vars, 'timeLeft': t.current});
+			setVars({...vars, 'timeLeft': vars.timeLeft - 1});
 	}
 
 	useEffect(() => {
+		// startTime indicates if the page was rendered from the host or re-rendered internally
+		// if the timer is re-rendered internally, startTime will not change because it is set when
+		// the component renders in the host
+		// 
+		// if the startTime changed that means this is a new render from the host, so reset the local state
+		// to the new state values
 		if(startTime != vars.startTime) {
 			setVars({'timeLeft': time, 'startTime': startTime});
-			t.current = time;
 		}
+		// if the startTime did not change, then this is an internal render so start the timer
 		else {
 			const timer = setTimeout(() => {
 				calculateTimeLeft();
@@ -31,7 +38,7 @@ const Timer = ({time, sendMessage, startTime}) => {
 
 	return (
 		<div>
-			<div>Timer: {vars.timeLeft}</div>
+			<div>{vars.timeLeft}</div>
 		</div>
 	)
 }
