b799acb16a0c065daca96f6268a7698898d1991d
pabaier <baierpa@g.cofc.edu>
2020-03-14 20:34:02 -0400

    redirect logout after action completes

--- frontend/src/components/joinForm.js
+++ frontend/src/components/joinForm.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Button, Col, Form } from 'react-bootstrap'
 import { useHistory } from "react-router-dom";
 
-const onSubmit = (e, history) => {
-	e.preventDefault();
-	const gameToken = e.target.elements.gameToken.value;
-	history.push(`/play/join/${gameToken}`);
-}
-
 const JoinForm = props => {
 	let history = useHistory();
+	
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const gameToken = e.target.elements.gameToken.value;
+		history.push(`/play/join/${gameToken}`);
+	}
 
 	return (
-		<Form onSubmit={ e => onSubmit(e, history)}>
+		<Form onSubmit={ e => onSubmit(e)}>
 			<Form.Row >
 					<Col>
 						<Form.Control type="text" name="gameToken" placeholder="Enter Game ID" />
