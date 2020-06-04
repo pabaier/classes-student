eaf1d1056fd00d85904d871cae86692d5fdd5fbe
pabaier <baierpa@g.cofc.edu>
2020-06-03 20:11:01 -0400

    add answer options to questions

--- frontend/src/components/questions/create.js
+++ frontend/src/components/questions/create.js
@@ -1,34 +1,62 @@
 import React, { useState } from 'react';
 import { Button, Form, Col } from 'react-bootstrap'
 
-const onSubmit = (e) => {
+const onSubmit = (e, key) => {
 	e.preventDefault();
 	const question = e.target.elements.question.value;
 	const category = e.target.elements.category.value;
+	// list of objects {'option': isAnswer(true/false)}
+	var answerOptions = []
+	for(var i=0; i < key; i++) {
+		var text = e.target.elements[`answerOption-${i}`].value;
+		var option = {};
+		option[text] = answers[i] ;
+		answerOptions.push(option);
+	}
 	console.log('submit', checked + " " + category + " " + question);
+	console.log('answerOptions', answerOptions);
 }
 
 const checkChange = (e) => {
 	checked = !checked
-}
+} 
 
 var checked = false;
+var answers = [false]
 
 const Create = () => {
+
+	const markAnswer = (e) => {
+		var key = parseInt(e.target.id.split('-')[1])
+		answers[key] = !answers[key];
+		console.log('answers', answers);
+	}
+
 	const [key, setKey] = useState(1)
 	const [answerOptions, setAnswerOptions] = useState([
 		<Form.Row key={0}>
-			<Form.Control type="text" placeholder="answer option" name="answerOption" />
+			<Col>
+				<Form.Control type="text" placeholder="answer option" name="answerOption-0" />
+			</Col>
+			<Col>
+				<Form.Check type="checkbox" label="Is Answer" id="isAnswer-0" onChange={markAnswer} />
+			</Col>
 		</Form.Row>
 	]);
 
 	const addAnswerOption = () => {
 		setAnswerOptions([...answerOptions, 
 			<Form.Row key={key}>
-				<Form.Control type="text" placeholder="answer option" name="answerOption" />
+				<Col>
+					<Form.Control type="text" placeholder="answer option" name={`answerOption-${key}`} />
+				</Col>
+				<Col>
+					<Form.Check inline type="checkbox" label="Is Answer" id={`isAnswer-${key}`} onChange={markAnswer} />
+				</Col>
 			</Form.Row>
 		]);
 		setKey(key + 1);
+		answers.push(false);
 	}
 
 	const removeAnswerOption = () => {
@@ -44,7 +72,7 @@ const Create = () => {
 		<>
 			<h3>Create Question</h3>
 
-			<Form onSubmit={ e => onSubmit(e)}>
+			<Form onSubmit={ e => onSubmit(e, key)}>
 				<Form.Row>
 					<Form.Control type="text" placeholder="question" name="question" />
 				</Form.Row>
